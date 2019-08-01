c76ebe9be3663baea093172ebec4d2a28cf1a081
Matt Gumbel <matt@soliddesign.net>
2019-08-01 19:00:51 -0400

    Update project configure for Eclipse

---
+++ src/org/etools/j1939_84/J1939_84.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019. Equipment & Tool Institute
+ */
+package org.etools.j1939_84;
+
+/**
+ * @author Matt Gumbel (matt@soliddesign.net)
+ *
+ */
+public class J1939_84 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is the start of the J1939-84 Tool.");
+	}
+
+}
