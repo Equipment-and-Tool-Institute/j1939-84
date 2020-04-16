b0bc74ef559f2c4dd2d066614cf807ad43a2bbab
garlandg <62403205+garlandg@users.noreply.github.com>
2020-04-16 12:06:03 -0400

    Updates for Step08Controller

---
+++ src-test/org/etools/j1939_84/controllers/part1/Step08ControllerTest.java
@@ -0,0 +1,272 @@
+/**
+ * Copyright 2020 Equipment & Tool Institute
+ */
+package org.etools.j1939_84.controllers.part1;
+
+import static org.etools.j1939_84.model.Outcome.FAIL;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
+import org.etools.j1939_84.controllers.ResultsListener;
+import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.FuelType;
+import org.etools.j1939_84.model.IgnitionType;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.PartResultFactory;
+import org.etools.j1939_84.model.VehicleInformation;
+import org.etools.j1939_84.modules.BannerModule;
+import org.etools.j1939_84.modules.DateTimeModule;
+import org.etools.j1939_84.modules.DiagnosticReadinessModule;
+import org.etools.j1939_84.modules.EngineSpeedModule;
+import org.etools.j1939_84.modules.ReportFileModule;
+import org.etools.j1939_84.modules.TestDateTimeModule;
+import org.etools.j1939_84.modules.VehicleInformationModule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ *
+ * @author Garrison Garland (garrison@test.soliddesign.net)
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class Step08ControllerTest {
+
+    @Mock
+    private AcknowledgmentPacket acknowledgmentPacket;
+
+    @Mock
+    private BannerModule bannerModule;
+
+    @Mock
+    private DataRepository dataRepository;
+
+    private DateTimeModule dateTimeModule;
+
+    @Mock
+    private DiagnosticReadinessModule diagnosticReadinessModule;
+
+    @Mock
+    private EngineSpeedModule engineSpeedModule;
+
+    @Mock
+    private Executor executor;
+
+    @Mock
+    private FuelType fuelType;
+
+    @Mock
+    private IgnitionType ignitionType;
+
+    private Step08Controller instance;
+
+    @Mock
+    private J1939 j1939;
+
+    private TestResultsListener listener;
+
+    @Mock
+    private ResultsListener mockListener;
+
+    @Mock
+    private PartResultFactory partResultFactory;
+
+    @Mock
+    private ReportFileModule reportFileModule;
+
+    @Mock
+    private VehicleInformationModule vehicleInformationModule;
+
+    private void runTest() {
+        instance.execute(listener, j1939, reportFileModule);
+        ArgumentCaptor<Runnable> runnableCaptor = ArgumentCaptor.forClass(Runnable.class);
+        verify(executor).execute(runnableCaptor.capture());
+        runnableCaptor.getValue().run();
+
+        verify(engineSpeedModule).setJ1939(j1939);
+        verify(vehicleInformationModule).setJ1939(j1939);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        listener = new TestResultsListener(mockListener);
+        dateTimeModule = new TestDateTimeModule();
+
+        instance = new Step08Controller(
+                executor,
+                engineSpeedModule,
+                bannerModule,
+                dateTimeModule,
+                vehicleInformationModule,
+                partResultFactory,
+                diagnosticReadinessModule,
+                dataRepository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        verifyNoMoreInteractions(executor,
+                engineSpeedModule,
+                bannerModule,
+                vehicleInformationModule,
+                partResultFactory,
+                diagnosticReadinessModule,
+                dataRepository,
+                mockListener,
+                reportFileModule);
+    }
+
+    @Test
+    public void testGetDiplayName() {
+        assertEquals("Part 1 Step 8", instance.getDisplayName());
+    }
+
+    @Test
+    public void testGetTotalSteps() {
+        assertEquals(1, instance.getTotalSteps());
+    }
+
+    @Test
+    public void testRun() {
+        List<DM20MonitorPerformanceRatioPacket> globalDM20s = new ArrayList<>();
+        when(diagnosticReadinessModule.getDM20Packets(any(), true)).thenReturn(globalDM20s);
+
+        OBDModuleInformation moduleInfo = mock(OBDModuleInformation.class);
+        when(dataRepository.getObdModule(0)).thenReturn(moduleInfo);
+
+        VehicleInformation vehicleInformation = new VehicleInformation();
+        vehicleInformation.setEmissionUnits(1);
+        when(dataRepository.getVehicleInformation()).thenReturn(vehicleInformation);
+
+        // when(IgnitionType.SPARK ==
+        // vehicleInformation.equals(ignitionType.SPARK)).thenReturn(true);
+
+        runTest();
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(diagnosticReadinessModule).setJ1939(j1939);
+        verify(diagnosticReadinessModule).getDM20Packets(any(), eq(true));
+        verify(dataRepository).getObdModule(0);
+        verify(dataRepository).getVehicleInformation();
+    }
+
+    @Test
+    public void toLittleSPNs() {
+        List<DM20MonitorPerformanceRatioPacket> globalDM20s = new ArrayList<>();
+        when(diagnosticReadinessModule.getDM20Packets(any(),
+                true)).thenReturn(globalDM20s);
+
+        OBDModuleInformation moduleInfo = mock(OBDModuleInformation.class);
+        when(dataRepository.getObdModule(0)).thenReturn(moduleInfo);
+
+        VehicleInformation vehicleInformation = new VehicleInformation();
+        vehicleInformation.setEmissionUnits(1);
+        when(dataRepository.getVehicleInformation()).thenReturn(vehicleInformation);
+
+        // when(IgnitionType.SPARK ==
+        // vehicleInformation.equals(ignitionType.SPARK)).thenReturn(true);
+
+        runTest();
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(diagnosticReadinessModule).setJ1939(j1939);
+        verify(diagnosticReadinessModule).getDM20Packets(any(), eq(true));
+        verify(dataRepository).getObdModule(0);
+        verify(dataRepository).getVehicleInformation();
+
+        verify(mockListener)
+                .addOutcome(1, 8, FAIL, "6.1.8.2.a - minimum expected SPNs for Diesel fuel type are not supported.");
+
+        verify(mockListener)
+                .addOutcome(1, 8, FAIL, "6.1.8.2.a - minimum expected SPNs for Spark Ignition are not supported.");
+    }
+
+    @Test
+    public void wrongFuelType() {
+        List<DM20MonitorPerformanceRatioPacket> globalDM20s = new ArrayList<>();
+        when(diagnosticReadinessModule.getDM20Packets(any(),
+                true)).thenReturn(globalDM20s);
+
+        OBDModuleInformation moduleInfo = mock(OBDModuleInformation.class);
+        when(dataRepository.getObdModule(0)).thenReturn(moduleInfo);
+
+        VehicleInformation vehicleInformation = new VehicleInformation();
+        vehicleInformation.setEmissionUnits(1);
+        when(dataRepository.getVehicleInformation()).thenReturn(vehicleInformation);
+
+        // when(IgnitionType.SPARK ==
+        // vehicleInformation.equals(ignitionType.SPARK)).thenReturn(true);
+
+        runTest();
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(1,
+                8,
+                FAIL,
+                "6.1.8.2.a - Fuel Type not supported in Monitor Performance Ratio Evaluation.");
+
+        verify(diagnosticReadinessModule).setJ1939(j1939);
+        verify(diagnosticReadinessModule).getDM20Packets(any(), eq(true));
+        verify(dataRepository).getObdModule(0);
+        verify(dataRepository).getVehicleInformation();
+    }
+
+    @Test
+    public void wrongIgnitionType() {
+        List<DM20MonitorPerformanceRatioPacket> globalDM20s = new ArrayList<>();
+        when(diagnosticReadinessModule.getDM20Packets(any(),
+                true)).thenReturn(globalDM20s);
+
+        OBDModuleInformation moduleInfo = mock(OBDModuleInformation.class);
+        when(dataRepository.getObdModule(0)).thenReturn(moduleInfo);
+
+        VehicleInformation vehicleInformation = new VehicleInformation();
+        vehicleInformation.setEmissionUnits(1);
+        when(dataRepository.getVehicleInformation()).thenReturn(vehicleInformation);
+
+        // when(IgnitionType.SPARK ==
+        // vehicleInformation.equals(ignitionType.SPARK)).thenReturn(true);
+
+        runTest();
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(1,
+                8,
+                FAIL,
+                "6.1.8.2.a - Ignition Type not supported in Monitor Performance Ratio Evaluation.");
+
+        verify(diagnosticReadinessModule).setJ1939(j1939);
+        verify(diagnosticReadinessModule).getDM20Packets(any(), eq(true));
+        verify(dataRepository).getObdModule(0);
+        verify(dataRepository).getVehicleInformation();
+    }
+
+}
---
+++ src/org/etools/j1939_84/controllers/part1/Step08Controller.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2020 Equipment & Tool Institute
+ */
+package org.etools.j1939_84.controllers.part1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
+import org.etools.j1939_84.controllers.Controller;
+import org.etools.j1939_84.model.FuelType;
+import org.etools.j1939_84.model.IgnitionType;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.PartResultFactory;
+import org.etools.j1939_84.model.VehicleInformation;
+import org.etools.j1939_84.modules.BannerModule;
+import org.etools.j1939_84.modules.DateTimeModule;
+import org.etools.j1939_84.modules.DiagnosticReadinessModule;
+import org.etools.j1939_84.modules.EngineSpeedModule;
+import org.etools.j1939_84.modules.VehicleInformationModule;
+
+public class Step08Controller extends Controller {
+
+    private final DataRepository dataRepository;
+
+    private final DiagnosticReadinessModule diagnosticReadinessModule;
+
+    Step08Controller(DataRepository dataRepository) {
+        this(Executors.newSingleThreadScheduledExecutor(), new EngineSpeedModule(), new BannerModule(),
+                new DateTimeModule(), new VehicleInformationModule(), new PartResultFactory(),
+                new DiagnosticReadinessModule(), dataRepository);
+    }
+
+    Step08Controller(Executor executor, EngineSpeedModule engineSpeedModule,
+            BannerModule bannerModule, DateTimeModule dateTimeModule,
+            VehicleInformationModule vehicleInformationModule, PartResultFactory partResultFactory,
+            DiagnosticReadinessModule diagnosticReadinessModule, DataRepository dataRepository) {
+        super(executor, engineSpeedModule, bannerModule, dateTimeModule, vehicleInformationModule, partResultFactory);
+        this.diagnosticReadinessModule = diagnosticReadinessModule;
+        this.dataRepository = dataRepository;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Part 1 Step 8";
+    }
+
+    @Override
+    protected int getTotalSteps() {
+        return 1;
+    }
+
+    @Override
+    protected void run() throws Throwable {
+        diagnosticReadinessModule.setJ1939(getJ1939());
+
+        // 6.1.8.1.a. Global DM20 (send Request (PGN 59904) for PGN 49664
+        List<DM20MonitorPerformanceRatioPacket> globalDM20s = diagnosticReadinessModule.getDM20Packets(getListener(),
+                true);
+
+        // 6.1.8.1.a.i. Create list of ECU address
+        List<Integer> ecuAddresses = new ArrayList<>();
+        for (DM20MonitorPerformanceRatioPacket packet : globalDM20s) {
+            int sourceAddress = packet.getSourceAddress();
+            OBDModuleInformation info = dataRepository.getObdModule(sourceAddress);
+            if (info != null) {
+                ecuAddresses.add(sourceAddress);
+            }
+        }
+
+        VehicleInformation vehicleInfo = dataRepository.getVehicleInformation();
+        DiagnosticReadinessModule.getRatios(globalDM20s);
+        vehicleInfo.getFuelType();
+
+        // create set of all SPNs (as integers) from all DM20s
+        Set<Integer> dm20Spns = globalDM20s.stream()
+                .flatMap(dm20 -> dm20.getRatios().stream())
+                .map(ratio -> ratio.getSpn())
+                .collect(Collectors.toSet());
+
+        if (FuelType.DSL == vehicleInfo.getFuelType()) {
+            // List of SPNs per Fuel type that is checked with the SPNs Stored in stream
+            // FIXME Last three SPNs for the NOx Catalyst Bank 1 or NOx Adsorber Don't all
+            // have to be in there.
+            // It can be any combination of the three.
+            int SPNd[] = { 5322, 5318, 3058, 3064, 5321, 3055 };
+            if (!IntStream.of(SPNd).allMatch(spn -> dm20Spns.contains(spn))) {
+                addFailure(1, 8, "6.1.8.2.a - minimum expected SPNs for Diesel fuel type are not supported.");
+            } else {
+                // TODO Send message that the proper SPNs were passed
+                System.out.println("All SPNs recieved.");
+            }
+        } else {
+
+            // FIXME add warning Unknown fuel type.
+            addFailure(1, 8, "6.1.8.2.a - Fuel Type not supported in Monitor Performance Ratio Evaluation.");
+        }
+        if (IgnitionType.SPARK != null) {
+            // TODO Add the Outlet Oxygen Sensor Banks in Table A-3-2 (pg.111) with
+            // non-integer variables
+            int SPNsi[] = { 3054, 3058, 3306, 3053, 3050, 3051, 3055, 3056, 3057 };
+            if (!IntStream.of(SPNsi).allMatch(spn -> dm20Spns.contains(spn))) {
+                addFailure(1, 8, "6.1.8.2.a - minimum expected SPNs for Spark Ignition are not supported.");
+            } else {
+                // TODO Send message that the proper SPNs were passed
+                System.out.println("All SPNs recieved.");
+            }
+        } else {
+
+            // FIXME add warning Unknown fuel type.
+            addFailure(1, 8, "6.1.8.2.a - Ignition Type not supported in Monitor Performance Ratio Evaluation.");
+        }
+    }
+}
