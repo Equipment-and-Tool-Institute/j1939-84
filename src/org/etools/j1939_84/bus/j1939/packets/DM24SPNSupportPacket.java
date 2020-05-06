fbe910f220d1a30538aef635420489d645322b52
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-05-06 10:54:54 -0400

    Fix #42 - update the toString() method to stop filtering out results as that was un-needed. (#45)

--- src/org/etools/j1939_84/bus/j1939/packets/DM24SPNSupportPacket.java
+++ src/org/etools/j1939_84/bus/j1939/packets/DM24SPNSupportPacket.java
@@ -18,71 +18,69 @@ import org.etools.j1939_84.bus.Packet;
  */
 public class DM24SPNSupportPacket extends ParsedPacket {
 
-	public static final int PGN = 64950;
+    public static final int PGN = 64950;
 
-	private List<SupportedSPN> spns;
+    private List<SupportedSPN> spns;
 
-	public DM24SPNSupportPacket(Packet packet) {
-		super(packet);
-	}
+    public DM24SPNSupportPacket(Packet packet) {
+        super(packet);
+    }
 
-	@Override
-	public String getName() {
-		return "DM24";
-	}
+    @Override
+    public String getName() {
+        return "DM24";
+    }
 
-	/**
-	 * Returns the {@link List} of {@link SupportedSPN}
-	 *
-	 * @return {@link List}
-	 */
-	public List<SupportedSPN> getSupportedSpns() {
-		if (spns == null) {
-			spns = new ArrayList<>();
-			parsePacket();
-		}
-		return spns;
-	}
+    /**
+     * Returns the {@link List} of {@link SupportedSPN}
+     *
+     * @return {@link List}
+     */
+    public List<SupportedSPN> getSupportedSpns() {
+        if (spns == null) {
+            spns = new ArrayList<>();
+            parsePacket();
+        }
+        return spns;
+    }
 
-	/**
-	 * Parses the packet to populate all the {@link SupportedSPN}s
-	 */
-	private void parsePacket() {
-		final int length = getPacket().getLength();
-		for (int i = 0; i + 3 < length; i = i + 4) {
-			final SupportedSPN parsedSpn = parseSpn(i);
-			if (parsedSpn.getSpn() != 0) {
-				spns.add(parsedSpn);
-			}
-		}
-	}
+    /**
+     * Parses the packet to populate all the {@link SupportedSPN}s
+     */
+    private void parsePacket() {
+        final int length = getPacket().getLength();
+        for (int i = 0; i + 3 < length; i = i + 4) {
+            final SupportedSPN parsedSpn = parseSpn(i);
+            if (parsedSpn.getSpn() != 0) {
+                spns.add(parsedSpn);
+            }
+        }
+    }
 
-	/**
-	 * Parses a portion of the packet to create a {@link SupportedSPN}
-	 *
-	 * @param bytes
-	 *              the bytes to parse
-	 * @param index
-	 *              the index at which the parsing starts
-	 * @return a {@link SupportedSPN}
-	 */
-	private SupportedSPN parseSpn(int index) {
-		int[] data = getPacket().getData(index, index + 4);
-		return new SupportedSPN(data);
-	}
+    /**
+     * Parses a portion of the packet to create a {@link SupportedSPN}
+     *
+     * @param bytes
+     *              the bytes to parse
+     * @param index
+     *              the index at which the parsing starts
+     * @return a {@link SupportedSPN}
+     */
+    private SupportedSPN parseSpn(int index) {
+        int[] data = getPacket().getData(index, index + 4);
+        return new SupportedSPN(data);
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getStringPrefix());
-		sb.append("(Supporting Scaled Test Results) [" + NL);
-		for (SupportedSPN spn : getSupportedSpns()) {
-			if (spn.supportsScaledTestResults()) {
-				sb.append("  ").append(spn).append(NL);
-			}
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getStringPrefix());
+        sb.append("(Supporting Test Results) [" + NL);
+        for (SupportedSPN spn : getSupportedSpns()) {
+            sb.append("  ").append(spn).append(NL);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 
 }
