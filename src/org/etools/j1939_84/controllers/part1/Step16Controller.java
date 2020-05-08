5509e893f405b3ef476d9ef8eab758ff368b7376
garlandg <62403205+garlandg@users.noreply.github.com>
2020-05-08 12:22:54 -0400

    Fix #21 Step 16 Beginning

--- src/org/etools/j1939_84/controllers/part1/Step16Controller.java
+++ src/org/etools/j1939_84/controllers/part1/Step16Controller.java
@@ -3,11 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part1;
 
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM2PreviouslyActiveDTC;
 import org.etools.j1939_84.controllers.Controller;
 import org.etools.j1939_84.model.PartResultFactory;
 import org.etools.j1939_84.modules.BannerModule;
+import org.etools.j1939_84.modules.DTCModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
@@ -18,10 +23,18 @@ import org.etools.j1939_84.modules.VehicleInformationModule;
  */
 public class Step16Controller extends Controller {
 
+    private final DTCModule dtcModule;
+
+    Step16Controller(DataRepository dataRepository) {
+        this(Executors.newSingleThreadScheduledExecutor(), new EngineSpeedModule(), new BannerModule(),
+                new DateTimeModule(), new VehicleInformationModule(), new DTCModule(), new PartResultFactory());
+    }
+
     protected Step16Controller(Executor executor, EngineSpeedModule engineSpeedModule, BannerModule bannerModule,
-            DateTimeModule dateTimeModule, VehicleInformationModule vehicleInformationModule,
+            DateTimeModule dateTimeModule, VehicleInformationModule vehicleInformationModule, DTCModule dtcmodule,
             PartResultFactory partResultFactory) {
         super(executor, engineSpeedModule, bannerModule, dateTimeModule, vehicleInformationModule, partResultFactory);
+        dtcModule = dtcmodule;
         // TODO Auto-generated constructor stub
     }
 
@@ -65,6 +78,17 @@ public class Step16Controller extends Controller {
          * a. Fail if NACK not received from OBD ECUs that did not respond to global
          * query.
          */
+
+        // 6.1.16.1.a. Global DM2 (send Request (PGN 59904) for PGN 65227
+        List<DM2PreviouslyActiveDTC> globalDM2s = dtcModule.requestDM2(listener).stream()
+                .filter(p -> p instanceof DM2PreviouslyActiveDTC)
+                .map(p -> (DM2PreviouslyActiveDTC) p)
+                .collect(Collectors.toList());
+        globalDM2s.get(0).getDtcs();
+        globalDM2s.get(0).getMalfunctionIndicatorLampStatus();
+        // LampStatus.OFF;
+        globalDM2s.get(0).getProtectLampStatus();
+
     }
 
 }
