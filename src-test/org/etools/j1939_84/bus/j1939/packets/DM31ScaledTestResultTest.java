1ae6d1189da1afecd697f2c8358d42e9202b84bc
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-06-19 13:21:37 -0400

    Fix #84 Add DM31ScaledTestResults message handling to the project.  Also, added global requestDM25 to the DTCModule class.

--- src-test/org/etools/j1939_84/bus/j1939/packets/DM31ScaledTestResultTest.java
+++ src-test/org/etools/j1939_84/bus/j1939/packets/DM31ScaledTestResultTest.java
@@ -6,6 +6,8 @@ package org.etools.j1939_84.bus.j1939.packets;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.etools.j1939_84.bus.Packet;
 import org.junit.Test;
 
@@ -19,27 +21,40 @@ public class DM31ScaledTestResultTest {
 
     @Test
     public void testEmptyDTCs() {
-        Packet packet = Packet.create(0,
+        Packet packet = Packet.create(DM31ScaledTestResults.PGN,
                 0);
         DM31ScaledTestResults instance = new DM31ScaledTestResults(packet);
         assertEquals("DM31", instance.getName());
-        assertTrue(instance.getDtcPackets().isEmpty());
-        assertEquals(0, instance.getDtcPackets().size());
+        assertTrue(instance.getDtcLampStatuses().isEmpty());
+        assertEquals(0, instance.getPacket().getBytes().length);
     }
 
     @Test
     public void testOneDTCs() {
-        Packet packet = Packet.create(0,
-                0,
-                0x31,
-                0x4E,
-                0x4B,
-                0x44,
+        int[] data = {
+                0x61,       // SPN least significant bit
+                0x02,       // SPN most significant bit
+                0x13,       // Failure mode indicator
+                0x81,
                 0x58,
-                0x34);
+                0x34
+        };
+        Packet packet = Packet.create(DM31ScaledTestResults.PGN,
+                0,
+                data);
         DM31ScaledTestResults instance = new DM31ScaledTestResults(packet);
-        assertEquals("DM31", instance.getName());
-        assertEquals(1, instance.getDtcPackets().size());
+        assertEquals(1, instance.getDtcLampStatuses().size());
+
+        DTCLampStatus dtcLampStatus0 = instance.getDtcLampStatuses().get(0);
+        DiagnosticTroubleCode actualDTC = dtcLampStatus0.getDtc();
+        assertEquals(0x0261, actualDTC.getSuspectParameterNumber());
+        assertEquals(0x01, actualDTC.getConversionMethod());
+        assertEquals(0x13, actualDTC.getFailureModeIndicator());
+        assertEquals(0x01, actualDTC.getOccurrenceCount());
+        assertEquals(LampStatus.SLOW_FLASH, dtcLampStatus0.getMalfunctionIndicatorLampStatus());
+        assertEquals(LampStatus.OTHER, dtcLampStatus0.getProtectLampStatus());
+        assertEquals(LampStatus.OTHER, dtcLampStatus0.getRedStopLampStatus());
+        assertEquals(LampStatus.OFF, dtcLampStatus0.getAmberWarningLampStatus());
     }
 
     @Test
@@ -49,29 +64,65 @@ public class DM31ScaledTestResultTest {
 
     @Test
     public void testThreeDTCs() {
-        Packet packet = Packet.create(0,
+        Packet packet = Packet.create(DM31ScaledTestResults.PGN,
                 0,
-                0x31,
-                0x4E,
-                0x4B,
-                0x44,
+                0x61,       // SPN least significant bit
+                0x02,       // SPN most significant bit
+                0x13,       // Failure mode indicator
+                0x81,
                 0x58,
                 0x34,
-                0x54,
-                0x58,
-                0x30,
-                0x45,
+
+                0x21,       // SPN least significant bit
+                0x06,       // SPN most significant bit
+                0x1F,       // Failure mode indicator
+                0x23,
                 0x4A,
                 0x34,
-                0x30,
-                0x37,
-                0x36,
-                0x36,
+
+                0xEE,       // SPN least significant bit
+                0x10,       // SPN most significant bit
+                0x04,       // Failure mode indicator
+                0x00,
                 0x37,
                 0x2A);
         DM31ScaledTestResults instance = new DM31ScaledTestResults(packet);
         assertEquals("DM31", instance.getName());
-        assertEquals(3, instance.getDtcPackets().size());
+        List<DTCLampStatus> lampStatuses = instance.getDtcLampStatuses();
+        assertEquals(0x03, lampStatuses.size());
+
+        DTCLampStatus lampStatus0 = lampStatuses.get(0);
+        DiagnosticTroubleCode dtc0 = lampStatus0.getDtc();
+        assertEquals(0x01, dtc0.getConversionMethod());
+        assertEquals(0x13, dtc0.getFailureModeIndicator());
+        assertEquals(0x01, dtc0.getOccurrenceCount());
+        assertEquals(0x261, dtc0.getSuspectParameterNumber());
+        assertEquals(LampStatus.SLOW_FLASH, lampStatus0.getMalfunctionIndicatorLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus0.getProtectLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus0.getRedStopLampStatus());
+        assertEquals(LampStatus.OFF, instance.getDtcLampStatuses().get(0).getAmberWarningLampStatus());
+
+        DTCLampStatus lampStatus1 = lampStatuses.get(1);
+        DiagnosticTroubleCode dtc1 = lampStatus1.getDtc();
+        assertEquals(0x0, dtc1.getConversionMethod());
+        assertEquals(0x1F, dtc1.getFailureModeIndicator());
+        assertEquals(0x23, dtc1.getOccurrenceCount());
+        assertEquals(0x0621, dtc1.getSuspectParameterNumber());
+        assertEquals(LampStatus.OFF, lampStatus1.getMalfunctionIndicatorLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus1.getProtectLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus1.getRedStopLampStatus());
+        assertEquals(LampStatus.OFF, lampStatus1.getAmberWarningLampStatus());
+
+        DTCLampStatus lampStatus2 = lampStatuses.get(2);
+        DiagnosticTroubleCode dtc2 = lampStatus2.getDtc();
+        assertEquals(0x00, dtc2.getConversionMethod());
+        assertEquals(0x04, dtc2.getFailureModeIndicator());
+        assertEquals(0x00, dtc2.getOccurrenceCount());
+        assertEquals(0x10EE, dtc2.getSuspectParameterNumber());
+        assertEquals(LampStatus.OTHER, lampStatus2.getMalfunctionIndicatorLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus2.getProtectLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus2.getRedStopLampStatus());
+        assertEquals(LampStatus.OTHER, lampStatus2.getAmberWarningLampStatus());
     }
 
 }
--- src/org/etools/j1939_84/bus/j1939/packets/DM31ScaledTestResults.java
+++ src/org/etools/j1939_84/bus/j1939/packets/DM31ScaledTestResults.java
@@ -4,7 +4,6 @@
 package org.etools.j1939_84.bus.j1939.packets;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.etools.j1939_84.bus.Packet;
@@ -17,10 +16,10 @@ import org.etools.j1939_84.bus.Packet;
  *
  * DTC to Lamp Association
  */
-public class DM31ScaledTestResults extends DiagnosticTroubleCodePacket {
+public class DM31ScaledTestResults extends ParsedPacket {
     // Hex value of PGN = 00A300
     public static final int PGN = 41728;
-    private List<DiagnosticTroubleCodePacket> dtcPackets;
+    private List<DTCLampStatus> dtcLampStatuses;
 
     public DM31ScaledTestResults(Packet packet) {
         super(packet);
@@ -29,11 +28,11 @@ public class DM31ScaledTestResults extends DiagnosticTroubleCodePacket {
     /**
      * @return the dtcPackets
      */
-    public List<DiagnosticTroubleCodePacket> getDtcPackets() {
-        if (dtcPackets == null) {
+    public List<DTCLampStatus> getDtcLampStatuses() {
+        if (dtcLampStatuses == null) {
             parsePacket();
         }
-        return dtcPackets;
+        return dtcLampStatuses;
     }
 
     @Override
@@ -46,11 +45,9 @@ public class DM31ScaledTestResults extends DiagnosticTroubleCodePacket {
      */
     private void parsePacket() {
         final int length = getPacket().getLength();
-        dtcPackets = new ArrayList<>();
-        for (int i = 0; i + 5 < length; i = i + 6) {
-            dtcPackets.add(new DiagnosticTroubleCodePacket(Packet.create(getPacket().getId(),
-                    getPacket().getSource(),
-                    Arrays.copyOfRange(getPacket().getBytes(), i, i + 6))));
+        dtcLampStatuses = new ArrayList<>();
+        for (int i = 0; i + 6 <= length; i = i + 6) {
+            dtcLampStatuses.add(new DTCLampStatus(getPacket().getData(i, i + 6)));
         }
     }
 
