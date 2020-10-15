9ec64e0d7638a4b27f2dffa0ec9c7f792c2f6669
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-10-15 18:35:24 -0400

    Add DM1 messaging class and its associated unit tests to the code base.

---
+++ src/org/etools/j1939_84/bus/j1939/packets/DM1ActiveDTCsPacket.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2019 Equipment & Tool Institute
+ */
+package org.etools.j1939_84.bus.j1939.packets;
+
+import org.etools.j1939_84.bus.Packet;
+
+/**
+ * The DM1 Active Diagnostic Trouble Codes
+ *
+ * @author Marianne Schaefer (marianne.m.schaefer@gmail.com)
+ *
+ */
+public class DM1ActiveDTCsPacket extends DiagnosticTroubleCodePacket {
+
+    public static final int PGN = 65226; // (00FECA16);
+
+    public DM1ActiveDTCsPacket(Packet packet) {
+        super(packet);
+    }
+
+    @Override
+    public String getName() {
+        return "DM1";
+    }
+
+}
