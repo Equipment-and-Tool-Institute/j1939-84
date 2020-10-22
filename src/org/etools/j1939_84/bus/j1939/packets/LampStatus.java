6fcd04864b6a11da526ef87db4546e1b6aac7be6
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-10-22 14:04:35 -0400

    Fix #195 add two additional  states of "alternate off" and "not supported" to implement Section A.8 from the requirements.

--- src/org/etools/j1939_84/bus/j1939/packets/LampStatus.java
+++ src/org/etools/j1939_84/bus/j1939/packets/LampStatus.java
@@ -10,7 +10,8 @@ package org.etools.j1939_84.bus.j1939.packets;
  *
  */
 public enum LampStatus {
-    FAST_FLASH("fast flash"), OFF("off"), ON("on"), OTHER("other"), SLOW_FLASH("slow flash");
+    ALTERNATE_OFF("alternate off"), FAST_FLASH("fast flash"), NOT_SUPPORTED("not supported"), OFF("off"), ON("on"),
+    OTHER("other"), SLOW_FLASH("slow flash");
 
     /**
      * Returns a {@link LampStatus} based upon the onOff and flash values as
@@ -24,17 +25,29 @@ public enum LampStatus {
      * @return {@link LampStatus}
      */
     public static LampStatus getStatus(int onOff, int flash) {
+        System.out.println("onOff is: " + onOff);
+        System.out.println("flash is: " + flash);
+        System.out.println("onOff & 0x03 is: " + (onOff & 0x03));
+        System.out.println("flash & 0x03 is: " + (flash & 0x03));
+
         boolean off = (onOff & 0x03) == 0x00;
         boolean on = (onOff & 0x03) == 0x01;
+        boolean notSupported = (onOff & 0x03) == 0x03;
 
         boolean slowFlash = (flash & 0x03) == 0x00;
         boolean fastFlash = (flash & 0x03) == 0x01;
         boolean dontFlash = (flash & 0x03) == 0x03;
 
         if (off) {
-            return LampStatus.OFF;
-        }
+            System.out.println("slow flash is: " + slowFlash);
+            if (slowFlash) {
+                return LampStatus.ALTERNATE_OFF;
 
+            }
+            System.out.println("returning the correct value");
+            return LampStatus.OFF;
+
+        }
         if (on) {
             if (slowFlash) {
                 return LampStatus.SLOW_FLASH;
@@ -45,6 +58,9 @@ public enum LampStatus {
             if (dontFlash) {
                 return LampStatus.ON;
             }
+        }
+        if (notSupported && dontFlash) {
+            return LampStatus.NOT_SUPPORTED;
         }
         return LampStatus.OTHER;
     }
