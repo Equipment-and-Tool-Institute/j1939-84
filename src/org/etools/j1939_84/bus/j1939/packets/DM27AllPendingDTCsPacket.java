21626b52dbd6de7bc180879c05b3865708d5ac92
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-11-10 14:30:07 -0500

    Fix #226 Adding DM27 messaging and its associated unit test to the project.

---
+++ src/org/etools/j1939_84/bus/j1939/packets/DM27AllPendingDTCsPacket.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2019 Equipment & Tool Institute
+ */
+package org.etools.j1939_84.bus.j1939.packets;
+
+import org.etools.j1939_84.bus.Packet;
+
+/**
+ * The {@link DiagnosticTroubleCodePacket} for the All Pending Diagnostic
+ * Trouble Codes (DM27)
+ *
+ * @author Marianne Schaefer (marianne.schaefer@gmail.com)
+ *
+ */
+public class DM27AllPendingDTCsPacket extends DiagnosticTroubleCodePacket {
+    public static final int PGN = 64898; // 0x00FD82
+
+    public DM27AllPendingDTCsPacket(Packet packet) {
+        super(packet);
+    }
+
+    @Override
+    public String getName() {
+        return "DM27";
+    }
+
+}
