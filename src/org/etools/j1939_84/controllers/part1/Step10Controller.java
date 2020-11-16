0b8bee087a2f69fdde24703d21c07a7b5f81d424
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2020-11-16 11:01:38 -0500

    Fix #203 & Fix #223.  Per feedback from Eric we needed to remove the pre/post verification os the Section A5 table as it doesn't make sense to do that verification here because the current state of the engine.

--- src/org/etools/j1939_84/controllers/part1/Step10Controller.java
+++ src/org/etools/j1939_84/controllers/part1/Step10Controller.java
@@ -10,10 +10,6 @@ import java.util.stream.Collectors;
 
 import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
 import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response;
-import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
-import org.etools.j1939_84.bus.j1939.packets.DM28PermanentEmissionDTCPacket;
-import org.etools.j1939_84.bus.j1939.packets.DM33EmissionIncreasingAuxiliaryEmissionControlDeviceActiveTime;
-import org.etools.j1939_84.bus.j1939.packets.EngineHoursPacket;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.model.PartResultFactory;
 import org.etools.j1939_84.modules.BannerModule;
@@ -40,26 +36,23 @@ public class Step10Controller extends StepController {
     private final DiagnosticReadinessModule diagnosticReadinessModule;
     private final DTCModule dtcModule;
     private final OBDTestsModule obdTestsModule;
-    private final SectionA5Verifier sectionA5Verifier;
 
     Step10Controller(DataRepository dataRepository) {
         this(Executors.newSingleThreadScheduledExecutor(), new EngineSpeedModule(), new BannerModule(),
                 new DateTimeModule(), new VehicleInformationModule(), new DTCModule(), new PartResultFactory(),
-                new DiagnosticReadinessModule(), new OBDTestsModule(), dataRepository,
-                new SectionA5Verifier(dataRepository));
+                new DiagnosticReadinessModule(), new OBDTestsModule(), dataRepository);
     }
 
     protected Step10Controller(Executor executor, EngineSpeedModule engineSpeedModule, BannerModule bannerModule,
             DateTimeModule dateTimeModule, VehicleInformationModule vehicleInformationModule, DTCModule dtcModule,
             PartResultFactory partResultFactory, DiagnosticReadinessModule diagnosticReadinessModule,
-            OBDTestsModule obdTestsModule, DataRepository dataRepository, SectionA5Verifier sectionA5Verifier) {
+            OBDTestsModule obdTestsModule, DataRepository dataRepository) {
         super(executor, engineSpeedModule, bannerModule, dateTimeModule, vehicleInformationModule, partResultFactory,
                 PART_NUMBER, STEP_NUMBER, TOTAL_STEPS);
         this.dataRepository = dataRepository;
         this.dtcModule = dtcModule;
         this.obdTestsModule = obdTestsModule;
         this.diagnosticReadinessModule = diagnosticReadinessModule;
-        this.sectionA5Verifier = sectionA5Verifier;
     }
 
     @Override
@@ -68,30 +61,7 @@ public class Step10Controller extends StepController {
         diagnosticReadinessModule.setJ1939(getJ1939());
         obdTestsModule.setJ1939(getJ1939());
 
-        sectionA5Verifier.setJ1939(getJ1939());
-        // Collect all OBD module address
-        List<Integer> obdModuleAddresses = dataRepository.getObdModuleAddresses().stream()
-                .collect(Collectors.toList());
-
-        // Collect our values for comparison after Diagnostic Data Clear/Reset
-        // message is sent
-        List<DM28PermanentEmissionDTCPacket> previousDM28Packets = dtcModule.requestDM28(getListener(), true)
-                .getPackets()
-                .stream()
-                .filter(t -> !t.getDtcs().isEmpty())
-                .collect(Collectors.toList());
-
-        List<DM20MonitorPerformanceRatioPacket> previousDM20Packets = diagnosticReadinessModule
-                .requestDM20(getListener(), true).getPackets();
-
-        List<DM33EmissionIncreasingAuxiliaryEmissionControlDeviceActiveTime> previousDM33Packets = obdModuleAddresses
-                .stream()
-                .flatMap(address -> dtcModule.requestDM33(getListener(), address).getPackets().stream())
-                .collect(Collectors.toList());
-
-        List<EngineHoursPacket> previousEngineHourPackets = getVehicleInformationModule()
-                .requestEngineHours(getListener()).getPackets().stream()
-                .filter(packet -> packet instanceof EngineHoursPacket).map(p -> p)
+        dataRepository.getObdModuleAddresses().stream()
                 .collect(Collectors.toList());
 
         // 6.1.10 DM11: Diagnostic Data Clear/Reset for Active DTCs
@@ -107,18 +77,6 @@ public class Step10Controller extends StepController {
         boolean nacked = globalDM11Packets.stream().anyMatch(packet -> packet.getResponse() == Response.NACK);
         if (nacked) {
             addWarning(1, 10, "6.1.10.3.a - The request for DM11 was NACK'ed");
-        }
-
-        // b. Fail if any diagnostic information in any ECU is not reset or
-        // starts out with unexpected values. Diagnostic information is defined
-        // in section A.5, Diagnostic Information Definition.
-        if (!sectionA5Verifier.verify(previousDM28Packets,
-                previousDM20Packets,
-                previousDM33Packets,
-                previousEngineHourPackets,
-                getListener())) {
-            addFailure(1, 10, "6.1.10.3.b - Fail if any diagnostic information in any ECU is not "
-                    + "reset or starts out with unexpected values.");
         }
 
         // 6.1.10.3 Warn criteria:
