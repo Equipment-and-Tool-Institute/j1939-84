ac468de5d9f17e2589c7abbeddf54a457be9e40a
Matt Gumbel <matt@soliddesign.net>
2020-11-29 09:53:16 -0500

    Add isVariablePeriod to PgnDefinition

--- src/org/etools/j1939_84/bus/j1939/packets/model/PgnDefinition.java
+++ src/org/etools/j1939_84/bus/j1939/packets/model/PgnDefinition.java
@@ -11,8 +11,24 @@ public class PgnDefinition {
     public int id = -1;
     public String label = "";
     public String acronym = "";
+
+    /**
+     * The broadcast period for the Parameter Group, in milliseconds.
+     * If this is on request, this value should be ignored.
+     * If the broadcast period if variable, this indicates the maximum period.
+     */
     public int broadcastPeriod = -1; // in milliseconds
+
+    /**
+     * Indicates the packet may be received more often that the broadcastPeriod indicates
+     */
+    public boolean isVariableBroadcast = false;
+
+    /**
+     * This indicates the Parameter Group will only be transmitted when requested.
+     */
     public boolean isOnRequest = false;
+
     public List<SpnDefinition> spnDefinitions = Collections.emptyList();
 
 }
