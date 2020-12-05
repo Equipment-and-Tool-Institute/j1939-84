d7d54259a9b0a335e9dedfa9f5b4ab387b271086
Matt Gumbel <matt@soliddesign.net>
2020-12-05 16:51:33 -0500

    Add INFO Outcome

--- src/org/etools/j1939_84/model/Outcome.java
+++ src/org/etools/j1939_84/model/Outcome.java
@@ -1,18 +1,17 @@
-/**
+/*
  * Copyright (c) 2019. Equipment & Tool Institute
  */
 package org.etools.j1939_84.model;
 
 /**
  * @author Matt Gumbel (matt@soliddesign.net)
- *
  */
 public enum Outcome {
-    ABORT("ABORT"), FAIL("FAIL"), INCOMPLETE("INCOMPLETE"), PASS("PASS"), WARN("WARN");
+    ABORT("ABORT"), FAIL("FAIL"), INFO("INFO"), INCOMPLETE("INCOMPLETE"), PASS("PASS"), WARN("WARN");
 
     private final String string;
 
-    private Outcome(String string) {
+    Outcome(String string) {
         this.string = string;
     }
 
