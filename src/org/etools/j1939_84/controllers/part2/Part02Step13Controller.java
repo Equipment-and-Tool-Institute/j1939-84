0d19c64e45675c2237f2373d2a60ccd38fe47b1b
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-01-29 09:46:55 -0500

    Fix #264 Add Part 02 Step 13 testing logic to the project along with the unit tests for the code written

--- src/org/etools/j1939_84/controllers/part2/Part02Step13Controller.java
+++ src/org/etools/j1939_84/controllers/part2/Part02Step13Controller.java
@@ -3,11 +3,17 @@
  */
 package org.etools.j1939_84.controllers.part2;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import org.etools.j1939_84.bus.j1939.packets.DM31DtcToLampAssociation;
+import org.etools.j1939_84.bus.j1939.packets.DTCLampStatus;
+import org.etools.j1939_84.bus.j1939.packets.LampStatus;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
+import org.etools.j1939_84.modules.DTCModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
@@ -19,6 +25,7 @@ public class Part02Step13Controller extends StepController {
     private static final int TOTAL_STEPS = 0;
 
     private final DataRepository dataRepository;
+    private final DTCModule dtcModule;
 
     Part02Step13Controller(DataRepository dataRepository) {
         this(Executors.newSingleThreadScheduledExecutor(),
@@ -26,7 +33,8 @@ public class Part02Step13Controller extends StepController {
              new BannerModule(),
              new VehicleInformationModule(),
              dataRepository,
-             DateTimeModule.getInstance());
+             DateTimeModule.getInstance(),
+             new DTCModule());
     }
 
     Part02Step13Controller(Executor executor,
@@ -34,7 +42,8 @@ public class Part02Step13Controller extends StepController {
                            BannerModule bannerModule,
                            VehicleInformationModule vehicleInformationModule,
                            DataRepository dataRepository,
-                           DateTimeModule dateTimeModule) {
+                           DateTimeModule dateTimeModule,
+                           DTCModule dtcModule) {
         super(executor,
               engineSpeedModule,
               bannerModule,
@@ -44,10 +53,28 @@ public class Part02Step13Controller extends StepController {
               STEP_NUMBER,
               TOTAL_STEPS);
         this.dataRepository = dataRepository;
+        this.dtcModule = dtcModule;
     }
 
     @Override
     protected void run() throws Throwable {
-       getListener().onResult("There are " + dataRepository.getObdModuleAddresses() + " OBD Modules");
+        dtcModule.setJ1939(getJ1939());
+        //6.2.13 DM31: DTC to Lamp Association
+        // 6.2.13.1 Actions:
+        //  a. DS DM31 (send Request (PGN 59904) for PGN 41728 (SPNs 1214-1215, 4113, 4117)) to each OBD ECU.
+        RequestResult<DM31DtcToLampAssociation> globalResponse = dtcModule.requestDM31(getListener());
+
+        // 6.2.13.2 Fail criteria (if supported):
+        boolean isMILon = globalResponse.getPackets().stream()
+                .flatMap(p -> p.getDtcLampStatuses().stream())
+                .map(DTCLampStatus::getMalfunctionIndicatorLampStatus)
+                .anyMatch(mil -> mil != LampStatus.OFF);
+        //  a. Fail if any ECU does not report MIL off. See section A.8 for allowed values.
+        if (isMILon) {
+            addFailure("6.2.13.2.a - An ECU response does not report MIL off");
+        }
+        //  b. Fail if NACK not received from OBD ECUs that did not provide DM31.
+        List<Integer> obdModuleAddresses = dataRepository.getObdModuleAddresses();
+        checkForNACKs(globalResponse.getPackets(), globalResponse.getAcks(), obdModuleAddresses, "6.2.13.2.b");
     }
 }
