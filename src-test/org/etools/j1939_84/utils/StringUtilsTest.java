bcb0910642e44b39886109fe8aac8cff8f35d9b4
Matt Gumbel <matt@soliddesign.net>
2021-02-01 20:05:12 -0500

    Fix #524 Null Serial Numbers can cause an error

--- src-test/org/etools/j1939_84/utils/StringUtilsTest.java
+++ src-test/org/etools/j1939_84/utils/StringUtilsTest.java
@@ -4,7 +4,7 @@
 
 package org.etools.j1939_84.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -15,5 +15,26 @@ public class StringUtilsTest {
         String input = "\u0000\u0000\u0000mes\u0000sage\u0000\u0000\u0000";
         String actual = StringUtils.stripLeadingAndTrailingNulls(input);
         assertEquals("mes\u0000sage", actual);
+
+        input = "\u0000\u0000\u0000mes\u0000sage";
+        actual = StringUtils.stripLeadingAndTrailingNulls(input);
+        assertEquals("mes\u0000sage", actual);
+
+        input = "mes\u0000sage\u0000\u0000\u0000";
+        actual = StringUtils.stripLeadingAndTrailingNulls(input);
+        assertEquals("mes\u0000sage", actual);
+
+        input = "mes\u0000sage";
+        actual = StringUtils.stripLeadingAndTrailingNulls(input);
+        assertEquals("mes\u0000sage", actual);
+
+        input = "message";
+        actual = StringUtils.stripLeadingAndTrailingNulls(input);
+        assertEquals("message", actual);
+
+        input = "\u0000\u0000\u0000";
+        actual = StringUtils.stripLeadingAndTrailingNulls(input);
+        assertEquals("", actual);
+
     }
 }
