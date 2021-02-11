6bcb87c333cc72fcdd821342a1b823e85ad1d51f
Matt Gumbel <matt@soliddesign.net>
2021-02-11 17:45:29 -0500

    Fix #278 Implement Part 3 Step 8 and clean up Part 1 Step 18

--- src/org/etools/j1939_84/controllers/part01/Part01Step18Controller.java
+++ src/org/etools/j1939_84/controllers/part01/Part01Step18Controller.java
@@ -3,16 +3,17 @@
  */
 package org.etools.j1939_84.controllers.part01;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
-import org.etools.j1939_84.bus.j1939.packets.LampStatus;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
-import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -29,8 +30,6 @@ public class Part01Step18Controller extends StepController {
     private static final int PART_NUMBER = 1;
     private static final int STEP_NUMBER = 18;
     private static final int TOTAL_STEPS = 0;
-
-    private final DataRepository dataRepository;
 
     Part01Step18Controller(DataRepository dataRepository) {
         this(Executors.newSingleThreadScheduledExecutor(),
@@ -50,46 +49,43 @@ public class Part01Step18Controller extends StepController {
                            DataRepository dataRepository,
                            DateTimeModule dateTimeModule) {
         super(executor,
-              engineSpeedModule,
               bannerModule,
+              dateTimeModule,
+              dataRepository,
+              engineSpeedModule,
               vehicleInformationModule,
               diagnosticMessageModule,
-              dateTimeModule,
               PART_NUMBER,
               STEP_NUMBER,
               TOTAL_STEPS);
-        this.dataRepository = dataRepository;
     }
 
     @Override
     protected void run() throws Throwable {
 
         // 6.1.18.1.a. Global DM12 for PGN 65236
-        RequestResult<DM12MILOnEmissionDTCPacket> globalResponse = getDiagnosticMessageModule().requestDM12(getListener());
+        var globalPackets = getDiagnosticMessageModule().requestDM12(getListener()).getPackets();
 
-        // 6.1.18.2.c. Fail if no OBD ECU provides DM12.
-        List<DM12MILOnEmissionDTCPacket> globalPackets = globalResponse.getPackets();
-        if (globalPackets.isEmpty()) {
+        // 6.1.18.2.a. Fail if any ECU reports active DTCs.
+        globalPackets.stream()
+                .filter(p -> !p.getDtcs().isEmpty())
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.1.18.2.a - " + moduleName + " reported active DTCs"));
+
+        // 6.1.18.2.b. Fail if any ECU does not report MIL off.
+        globalPackets.stream()
+                .filter(p -> p.getMalfunctionIndicatorLampStatus() != OFF)
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.1.18.2.b - " + moduleName + " did not report MIL off"));
+
+        boolean obdModuleResponded = globalPackets.stream()
+                .anyMatch(p -> getDataRepository().isObdModule(p.getSourceAddress()));
+        if (!obdModuleResponded) {
+            // 6.1.18.2.c. Fail if no OBD ECU provides DM12.
             addFailure("6.1.18.2.c - No OBD ECU provided DM12");
-        } else {
-            for (DM12MILOnEmissionDTCPacket packet : globalPackets) {
-                // 6.1.18.2.a. Fail if any ECU reports active DTCs.
-                if (!packet.getDtcs().isEmpty()) {
-                    addFailure("6.1.18.2.a - An ECU reported active DTCs");
-                    break;
-                }
-            }
-
-            for (DM12MILOnEmissionDTCPacket packet : globalPackets) {
-                // 6.1.18.2.b. Fail if any ECU does not report MIL off.
-                if (packet.getMalfunctionIndicatorLampStatus() != LampStatus.OFF) {
-                    addFailure("6.1.18.2.b - An ECU did not report MIL off");
-                    break;
-                }
-            }
         }
 
-        List<Integer> obdModuleAddresses = dataRepository.getObdModuleAddresses();
+        List<Integer> obdModuleAddresses = getDataRepository().getObdModuleAddresses();
 
         // 6.1.18.3.a. DS DM12 to all OBD ECUs.
         List<BusResult<DM12MILOnEmissionDTCPacket>> dsResults = obdModuleAddresses
