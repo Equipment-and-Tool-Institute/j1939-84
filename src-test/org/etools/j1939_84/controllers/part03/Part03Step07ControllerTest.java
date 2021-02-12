316770a92c91c6473ac7318a8d70a3a74d5c38cd
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-12 09:55:31 -0500

    Fix #276 Implement Part 3 Step 7 (#552)

--- src-test/org/etools/j1939_84/controllers/part03/Part03Step07ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part03/Part03Step07ControllerTest.java
@@ -3,15 +3,30 @@
  */
 package org.etools.j1939_84.controllers.part03;
 
+import static org.etools.j1939_84.J1939_84.NL;
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.concurrent.Executor;
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM2PreviouslyActiveDTC;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,7 +134,154 @@ public class Part03Step07ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testRun() {
+    public void testNoFailures() {
 
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(new BusResult<>(false, nack));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
     }
+
+    @Test
+    public void testFailureForNoNack() {
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(new BusResult<>(true));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals(
+                "FAIL: 6.3.7.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query" + NL,
+                listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.3.7.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
+    }
+
+    @Test
+    public void testFailureForDTC() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("FAIL: 6.3.7.2.a - OBD ECU Engine #1 (0) reported a previously active DTC" + NL,
+                     listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.3.7.2.a - OBD ECU Engine #1 (0) reported a previously active DTC");
+    }
+
+    @Test
+    public void testFailureForMILNotOff() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("FAIL: 6.3.7.2.b - OBD ECU Engine #1 (0) did not report MIL off" + NL, listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.3.7.2.b - OBD ECU Engine #1 (0) did not report MIL off");
+    }
+
+    @Test
+    public void testFailureForNonOBDMilNotOff() {
+        var dm2 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals(
+                "FAIL: 6.3.7.2.c - Non-OBD ECU Engine #1 (0) did not report MIL off or not supported" + NL,
+                listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.3.7.2.c - Non-OBD ECU Engine #1 (0) did not report MIL off or not supported");
+    }
+
+    @Test
+    public void testFailureGlobalAndDSDifference() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var globalDM2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, globalDM2));
+
+        var dsDM2 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dsDM2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("FAIL: 6.3.7.4.a - Difference compared to data received during global request from Engine #1 (0)" + NL,
+                     listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.3.7.4.a - Difference compared to data received during global request from Engine #1 (0)");
+    }
+
 }
