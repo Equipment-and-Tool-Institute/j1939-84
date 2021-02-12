7020e17f92c961c61e81ad34bdcb42737ef593bc
Matt Gumbel <matt@soliddesign.net>
2021-02-11 20:42:37 -0500

    Fix #279 Implement Part 3 Step 10 and Update Part 1 Step 19

--- src-test/org/etools/j1939_84/controllers/part01/Part01Step19ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part01/Part01Step19ControllerTest.java
@@ -4,6 +4,7 @@
 package org.etools.j1939_84.controllers.part01;
 
 import static org.etools.j1939_84.J1939_84.NL;
+import static org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket.PGN;
 import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -13,21 +14,20 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.Executor;
 import org.etools.j1939_84.bus.Packet;
 import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
 import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
-import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
 import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
-import org.etools.j1939_84.modules.DiagnosticMessageModule;
 import org.etools.j1939_84.modules.DateTimeModule;
+import org.etools.j1939_84.modules.DiagnosticMessageModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.ReportFileModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
@@ -47,13 +47,11 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class Part01Step19ControllerTest extends AbstractControllerTest {
     private static final int PART_NUMBER = 1;
-    private static final int PGN = DM12MILOnEmissionDTCPacket.PGN;
     private static final int STEP_NUMBER = 19;
 
     @Mock
     private BannerModule bannerModule;
 
-    @Mock
     private DataRepository dataRepository;
 
     @Mock
@@ -85,6 +83,7 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
     public void setUp() {
         listener = new TestResultsListener(mockListener);
         DateTimeModule.setInstance(null);
+        dataRepository = DataRepository.newInstance();
 
         instance = new Part01Step19Controller(
                 executor,
@@ -95,7 +94,14 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
                 dataRepository,
                 DateTimeModule.getInstance());
 
-        setup(instance, listener, j1939, engineSpeedModule, reportFileModule, executor, vehicleInformationModule);
+        setup(instance,
+              listener,
+              j1939,
+              executor,
+              reportFileModule,
+              engineSpeedModule,
+              vehicleInformationModule,
+              diagnosticMessageModule);
     }
 
     @After
@@ -104,36 +110,44 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
                                  engineSpeedModule,
                                  bannerModule,
                                  vehicleInformationModule,
-                                 dataRepository,
                                  diagnosticMessageModule,
                                  mockListener);
     }
 
-    /**
-     * Test method for
-     * {@link Part01Step19Controller#run()} with
-     * no response to any request.
-     */
+    @Test
+    public void testGetDisplayName() {
+        String name = "Part " + PART_NUMBER + " Step " + STEP_NUMBER;
+        assertEquals("Display Name", name, instance.getDisplayName());
+    }
+
+    @Test
+    public void testGetStepNumber() {
+        assertEquals(STEP_NUMBER, instance.getStepNumber());
+    }
+
+    @Test
+    public void testGetTotalSteps() {
+        assertEquals("Total Steps", 0, instance.getTotalSteps());
+    }
+
     @Test
     public void testEmptyPacketFailure() {
-        List<Integer> obdModuleAddresses = List.of(0x01);
-        when(dataRepository.getObdModuleAddresses()).thenReturn(obdModuleAddresses);
 
-        when(diagnosticMessageModule.requestDM23(any()))
-                .thenReturn(new RequestResult<>(false, List.of(), List.of()));
-        when(diagnosticMessageModule.requestDM23(any(), eq(0x01)))
-                .thenReturn(new BusResult<>(false, Optional.empty()));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        when(diagnosticMessageModule.requestDM23(any())).thenReturn(new RequestResult<>(false));
+        when(diagnosticMessageModule.requestDM23(any(), eq(0x01))).thenReturn(new BusResult<>(false));
 
         runTest();
-        verify(dataRepository).getObdModuleAddresses();
 
-        verify(diagnosticMessageModule).setJ1939(j1939);
         verify(diagnosticMessageModule).requestDM23(any());
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x01));
 
         verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
                                         "6.1.19.2.c - No OBD ECU provided DM23");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
                                         "6.1.19.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
 
         String expectedResults = "FAIL: 6.1.19.2.c - No OBD ECU provided DM23" + NL;
@@ -143,25 +157,21 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
         assertEquals("", listener.getMilestones());
     }
 
-    /**
-     * Test method for
-     * {@link Part01Step19Controller##run()}
-     * with the packets in error condition.
-     */
     @Test
     public void testFailures() {
         DM23PreviouslyMILOnEmissionDTCPacket packet1 = new DM23PreviouslyMILOnEmissionDTCPacket(
                 Packet.create(PGN, 0x01, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88));
         DM23PreviouslyMILOnEmissionDTCPacket packet3 = new DM23PreviouslyMILOnEmissionDTCPacket(
                 Packet.create(PGN, 0x03, 0x00, 0x00, 0x04, 0x00, 0xFF, 0xFF, 0xFF, 0xFF));
-        List<Integer> obdModuleAddresses = List.of(1,3);
-        when(dataRepository.getObdModuleAddresses()).thenReturn(obdModuleAddresses);
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        dataRepository.putObdModule(new OBDModuleInformation(3));
 
         DM23PreviouslyMILOnEmissionDTCPacket obdPacket3 = new DM23PreviouslyMILOnEmissionDTCPacket(
                 Packet.create(PGN, 0x03, 0x11, 0x22, 0x13, 0x44, 0x55, 0x66, 0x77, 0x88));
 
         when(diagnosticMessageModule.requestDM23(any()))
-                .thenReturn(new RequestResult<>(false, List.of(packet1, packet3), List.of()));
+                .thenReturn(new RequestResult<>(false, packet1, packet3));
 
         when(diagnosticMessageModule.requestDM23(any(), eq(0x01)))
                 .thenReturn(new BusResult<>(false, packet1));
@@ -169,60 +179,34 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
                 .thenReturn(new BusResult<>(false, obdPacket3));
 
         runTest();
-        verify(dataRepository).getObdModuleAddresses();
 
-        verify(diagnosticMessageModule).setJ1939(j1939);
         verify(diagnosticMessageModule).requestDM23(any());
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x01));
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x03));
 
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                                  "6.1.19.2.a - An ECU reported active DTCs");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                                  "6.1.19.2.b - An ECU did not report MIL off");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                        "6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)");
-
-        String expectedResults = "FAIL: 6.1.19.2.a - An ECU reported active DTCs" + NL;
-        expectedResults += "FAIL: 6.1.19.2.b - An ECU did not report MIL off" + NL;
+        String expectedResults = "";
+        expectedResults += "FAIL: 6.1.19.2.a - Engine #2 (1) reported active DTCs" + NL;
+        expectedResults += "FAIL: 6.1.19.2.a - Transmission #1 (3) reported active DTCs" + NL;
+        expectedResults += "FAIL: 6.1.19.2.b - Engine #2 (1) did not report MIL off" + NL;
+        expectedResults += "FAIL: 6.1.19.2.b - Transmission #1 (3) did not report MIL off" + NL;
         expectedResults += "FAIL: 6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)" + NL;
 
         assertEquals(expectedResults, listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.a - Engine #2 (1) reported active DTCs");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.a - Transmission #1 (3) reported active DTCs");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.b - Engine #2 (1) did not report MIL off");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.b - Transmission #1 (3) did not report MIL off");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)");
     }
 
-    /**
-     * Test method for
-     * {@link org.etools.j1939_84.controllers.StepController#getDisplayName()}.
-     */
-    @Test
-    public void testGetDisplayName() {
-        String name = "Part " + PART_NUMBER + " Step " + STEP_NUMBER;
-        assertEquals("Display Name", name, instance.getDisplayName());
-    }
-
-    /**
-     * Test method for
-     * {@link Part01Step13Controller#getStepNumber()}.
-     */
-    @Test
-    public void testGetStepNumber() {
-        assertEquals(STEP_NUMBER, instance.getStepNumber());
-    }
-
-    /**
-     * Test method for
-     * {@link org.etools.j1939_84.controllers.StepController#getTotalSteps()}.
-     */
-    @Test
-    public void testGetTotalSteps() {
-        assertEquals("Total Steps", 0, instance.getTotalSteps());
-    }
-
-    /**
-     * Test method for
-     * {@link Part01Step19Controller#run()} with
-     * 'ACKs.
-     */
     @Test
     public void testMoreFailures() {
         AcknowledgmentPacket ackPacket = new AcknowledgmentPacket(
@@ -234,8 +218,8 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
                 Packet.create(PGN, 0x03, 0x11, 0x22, (byte) 0x0A, 0x44, 0x55, 0x66, 0x77,
                               0x88));
 
-        List<Integer> obdModuleAddresses = List.of(0x01, 0x03);
-        when(dataRepository.getObdModuleAddresses()).thenReturn(obdModuleAddresses);
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        dataRepository.putObdModule(new OBDModuleInformation(3));
 
         DM23PreviouslyMILOnEmissionDTCPacket packet3b = new DM23PreviouslyMILOnEmissionDTCPacket(
                 Packet.create(PGN, 0x03, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF));
@@ -248,24 +232,13 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
                 .thenReturn(new BusResult<>(false, packet3b));
 
         runTest();
-        verify(dataRepository).getObdModuleAddresses();
 
-        verify(diagnosticMessageModule).setJ1939(j1939);
         verify(diagnosticMessageModule).requestDM23(any());
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x01));
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x03));
 
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                        "6.1.19.2.a - An ECU reported active DTCs");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                        "6.1.19.2.b - An ECU did not report MIL off");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                        "6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
-                                        "6.1.19.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
-
-        String expectedResults = "FAIL: 6.1.19.2.a - An ECU reported active DTCs" + NL;
-        expectedResults += "FAIL: 6.1.19.2.b - An ECU did not report MIL off" + NL;
+        String expectedResults = "FAIL: 6.1.19.2.a - Transmission #1 (3) reported active DTCs" + NL;
+        expectedResults += "FAIL: 6.1.19.2.b - Transmission #1 (3) did not report MIL off" + NL;
         expectedResults += "FAIL: 6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)"
                 + NL;
         expectedResults += "FAIL: 6.1.19.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query"
@@ -274,31 +247,36 @@ public class Part01Step19ControllerTest extends AbstractControllerTest {
         assertEquals(expectedResults, listener.getResults());
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getMilestones());
+
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.a - Transmission #1 (3) reported active DTCs");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL,
+                                        "6.1.19.2.b - Transmission #1 (3) did not report MIL off");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.1.19.4.a - Difference compared to data received during global request from Transmission #1 (3)");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.1.19.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
     }
 
-    /**
-     * Test method for
-     * {@link Part01Step19Controller#run()} with
-     * non-error packets.
-     */
     @Test
     public void testNoErrors() {
 
         DM23PreviouslyMILOnEmissionDTCPacket packet1 = new DM23PreviouslyMILOnEmissionDTCPacket(
                 Packet.create(PGN, 0x01, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00));
 
-        List<Integer> obdModuleAddresses = List.of(0x01);
-        when(dataRepository.getObdModuleAddresses()).thenReturn(obdModuleAddresses);
+        dataRepository.putObdModule(new OBDModuleInformation(1));
 
         when(diagnosticMessageModule.requestDM23(any()))
-                .thenReturn(new RequestResult<>(false, List.of(packet1), List.of()));
+                .thenReturn(new RequestResult<>(false, packet1));
         when(diagnosticMessageModule.requestDM23(any(), eq(0x01)))
                 .thenReturn(new BusResult<>(false, packet1));
 
         runTest();
-        verify(dataRepository).getObdModuleAddresses();
 
-        verify(diagnosticMessageModule).setJ1939(j1939);
         verify(diagnosticMessageModule).requestDM23(any());
         verify(diagnosticMessageModule).requestDM23(any(), eq(0x01));
 
--- src/org/etools/j1939_84/controllers/part01/Part01Step19Controller.java
+++ src/org/etools/j1939_84/controllers/part01/Part01Step19Controller.java
@@ -3,20 +3,18 @@
  */
 package org.etools.j1939_84.controllers.part01;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
-import org.etools.j1939_84.bus.j1939.BusResult;
-import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
-import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
-import org.etools.j1939_84.bus.j1939.packets.LampStatus;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
-import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
-import org.etools.j1939_84.modules.DiagnosticMessageModule;
 import org.etools.j1939_84.modules.DateTimeModule;
+import org.etools.j1939_84.modules.DiagnosticMessageModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
 
@@ -25,14 +23,11 @@ import org.etools.j1939_84.modules.VehicleInformationModule;
  * <p>
  * The controller for 6.1.19 DM23: Emission Related Previously Active DTCs
  */
-
 public class Part01Step19Controller extends StepController {
 
     private static final int PART_NUMBER = 1;
     private static final int STEP_NUMBER = 19;
     private static final int TOTAL_STEPS = 0;
-
-    private final DataRepository dataRepository;
 
     Part01Step19Controller(DataRepository dataRepository) {
         this(Executors.newSingleThreadScheduledExecutor(),
@@ -52,56 +47,52 @@ public class Part01Step19Controller extends StepController {
                            DataRepository dataRepository,
                            DateTimeModule dateTimeModule) {
         super(executor,
-              engineSpeedModule,
               bannerModule,
+              dateTimeModule,
+              dataRepository,
+              engineSpeedModule,
               vehicleInformationModule,
               diagnosticMessageModule,
-              dateTimeModule,
               PART_NUMBER,
               STEP_NUMBER,
               TOTAL_STEPS);
-        this.dataRepository = dataRepository;
     }
-
 
     @Override
     protected void run() throws Throwable {
+        // 6.1.19.1.a. Global DM23 for PGN 65236
+        var globalPackets = getDiagnosticMessageModule().requestDM23(getListener()).getPackets();
 
-        // 6.1.19.1.a. Global DM23 (send Request (PGN 59904) for PGN 64949 (SPNs 1213-1215, 3038, 1706)).
-        RequestResult<DM23PreviouslyMILOnEmissionDTCPacket> globalResponse = getDiagnosticMessageModule().requestDM23(getListener());
-        // 6.1.19.1.c. Fail if no OBD ECU provides DM23.
-        List<DM23PreviouslyMILOnEmissionDTCPacket> globalPackets = globalResponse.getPackets();
-        if (globalPackets.isEmpty()) {
+        // 6.1.19.2.a. Fail if any ECU reports active DTCs.
+        globalPackets.stream()
+                .filter(p -> !p.getDtcs().isEmpty())
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.1.19.2.a - " + moduleName + " reported active DTCs"));
+
+        // 6.1.19.2.b. Fail if any ECU does not report MIL off.
+        globalPackets.stream()
+                .filter(p -> p.getMalfunctionIndicatorLampStatus() != OFF)
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.1.19.2.b - " + moduleName + " did not report MIL off"));
+
+        boolean obdModuleResponded = globalPackets.stream()
+                .anyMatch(p -> getDataRepository().isObdModule(p.getSourceAddress()));
+        if (!obdModuleResponded) {
+            // 6.1.19.2.c. Fail if no OBD ECU provides DM23.
             addFailure("6.1.19.2.c - No OBD ECU provided DM23");
-        } else {
-            for (DM23PreviouslyMILOnEmissionDTCPacket packet : globalPackets) {
-                // 6.1.19.2.a. Fail if any ECU reports previously active DTCs.
-                if (!packet.getDtcs().isEmpty()) {
-                    addFailure("6.1.19.2.a - An ECU reported active DTCs");
-                    break;
-                }
-            }
-            for (DM23PreviouslyMILOnEmissionDTCPacket packet : globalPackets) {
-                // 6.1.19.2.b. Fail if any ECU does not report MIL off.
-                if (packet.getMalfunctionIndicatorLampStatus() != LampStatus.OFF) {
-                    addFailure("6.1.19.2.b - An ECU did not report MIL off");
-                    break;
-                }
-            }
         }
 
-        // 6.1.19.3.a. DS DM23 to each OBD ECU.
-        List<Integer> obdModuleAddresses = dataRepository.getObdModuleAddresses();
-        List<BusResult<DM23PreviouslyMILOnEmissionDTCPacket>> dsResults = obdModuleAddresses.stream()
-                .map(address -> getDiagnosticMessageModule().requestDM23(getListener(), address))
+        List<Integer> obdModuleAddresses = getDataRepository().getObdModuleAddresses();
+
+        // 6.1.19.3.a. DS DM23 to all OBD ECUs.
+        var dsResults = obdModuleAddresses
+                .stream().map(address -> getDiagnosticMessageModule().requestDM23(getListener(), address))
                 .collect(Collectors.toList());
 
         // 6.1.19.4.a. Fail if any difference compared to data received during global request.
-        List<DM23PreviouslyMILOnEmissionDTCPacket> dsPackets = filterPackets(dsResults);
-        compareRequestPackets(globalPackets, dsPackets, "6.1.19.4.a");
+        compareRequestPackets(globalPackets, filterPackets(dsResults), "6.1.19.4.a");
 
-        // 6.1.19.4.b Fail if NACK not received from OBD ECUs that did not respond to global query
-        List<AcknowledgmentPacket> dsAcks = filterAcks(dsResults);
-        checkForNACKs(globalPackets, dsAcks, obdModuleAddresses, "6.1.19.4.b");
+        // 6.1.19.4.b. Fail if NACK not received from OBD ECUs that did not respond to global query.
+        checkForNACKs(globalPackets, filterAcks(dsResults), obdModuleAddresses, "6.1.19.4.b");
     }
 }
