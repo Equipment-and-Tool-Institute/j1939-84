e7419e169b48bec0cf152ac17330dad0c3fa1c9e
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-02-17 15:28:32 -0500

    Fix #286 - Add part 4 step 1 and its associated unit tests to the project.

--- src/org/etools/j1939_84/controllers/part04/Part04Step01Controller.java
+++ src/org/etools/j1939_84/controllers/part04/Part04Step01Controller.java
@@ -54,6 +54,7 @@ public class Part04Step01Controller extends StepController {
     protected void run() throws Throwable {
         // 6.4.1.1.a. Gather broadcast data for engine speed (e.g., SPN 190).
         // 6.4.1.2.a. If engine speed is <400 rpm, prompt/warn operator to confirm engine is running and then press enter.
+        ensureKeyOnEngineOn();
     }
 
 }
