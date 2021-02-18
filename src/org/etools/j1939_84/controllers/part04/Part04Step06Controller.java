dbe1512d6a8c22dcf447ad96a4f09769d6a0198d
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-18 16:21:44 -0500

    Fix #291 Implement Part 4 Step 6 (#589)

--- src/org/etools/j1939_84/controllers/part04/Part04Step06Controller.java
+++ src/org/etools/j1939_84/controllers/part04/Part04Step06Controller.java
@@ -3,10 +3,18 @@
  */
 package org.etools.j1939_84.controllers.part04;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import org.etools.j1939_84.bus.j1939.packets.DM1ActiveDTCsPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM5DiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -53,9 +61,46 @@ public class Part04Step06Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.4.6.1.a Global DM5 [(send Request (PGN 59904) for PGN 65230 (SPNs 1218-1219)]).
+        var packets = getDiagnosticMessageModule().requestDM5(getListener())
+                .getPackets()
+                .stream()
+                .filter(p -> getDataRepository().isObdModule(p.getSourceAddress()))
+                .collect(Collectors.toList());
+
         // 6.4.6.2.a Fail if no OBD ECU reports number of active DTCs as > 0.
+        boolean noActiveCodes = packets.stream()
+                .map(DM5DiagnosticReadinessPacket::getActiveCodeCount)
+                .allMatch(c -> c == 0);
+        if (noActiveCodes) {
+            addFailure("6.4.6.2.a - No OBD ECU reported number of active DTCs as > 0");
+        }
+
         // 6.4.6.2.b Fail if any OBD ECU reports a different number of active DTCs than it did in DM1 response earlier in this part.
+        packets.stream()
+                .filter(p -> p.getActiveCodeCount() != getDTCs(p.getSourceAddress()).size())
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.4.6.2.b - " + moduleName + " reported a different number " +
+                                                          "of active DTCs than it did in DM1 response earlier in this part."));
+
         // 6.4.6.2.c Fail if any OBD ECU reports > 0 previously active DTCs.
+        packets.stream()
+                .filter(p -> p.getPreviouslyActiveCodeCount() > 0)
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.4.6.2.c - " + moduleName + " reported > 0 previously active DTCs"));
+    }
+
+    private List<DiagnosticTroubleCode> getDTCs(int moduleAddress) {
+        List<DiagnosticTroubleCode> results = new ArrayList<>();
+
+        OBDModuleInformation obdModuleInformation = getDataRepository().getObdModule(moduleAddress);
+        if (obdModuleInformation != null) {
+            DM1ActiveDTCsPacket packet = obdModuleInformation.get(DM1ActiveDTCsPacket.class);
+            if (packet != null) {
+                results.addAll(packet.getDtcs());
+            }
+        }
+
+        return results;
     }
 
 }
