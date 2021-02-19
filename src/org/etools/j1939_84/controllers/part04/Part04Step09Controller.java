c07b00973cc877725c62cf56ce335304700d02a1
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-19 09:41:22 -0500

    Implement Part 4 Step 9 (#592)

--- src/org/etools/j1939_84/controllers/part04/Part04Step09Controller.java
+++ src/org/etools/j1939_84/controllers/part04/Part04Step09Controller.java
@@ -3,10 +3,17 @@
  */
 package org.etools.j1939_84.controllers.part04;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCodePacket;
+import org.etools.j1939_84.bus.j1939.packets.LampStatus;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -52,12 +59,43 @@ public class Part04Step09Controller extends StepController {
 
     @Override
     protected void run() throws Throwable {
-        // 6.4.9.1.a Global DM27 [(send Request (PGN 59904) for PGN 64898 (SPNs 1213-1215, 3038, 1706)]).
+
+        var globalPackets = getDiagnosticMessageModule().requestDM27(getListener()).getPackets();
+
         // 6.4.9.2.a (if supported) Fail if any ECU reports a pending DTC.
+        globalPackets.stream()
+                .filter(p -> !p.getDtcs().isEmpty())
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.4.9.2.a - " + moduleName + " reported a pending DTC"));
+
         // 6.4.9.2.b (if supported) Fail if any ([OBD)] ECU reports a different MIL status than it did for DM12 response earlier in this part.
+        globalPackets.stream()
+                .filter(p -> getLampStatus(p.getSourceAddress()) != null)
+                .filter(p -> p.getMalfunctionIndicatorLampStatus() != getLampStatus(p.getSourceAddress()))
+                .map(ParsedPacket::getModuleName)
+                .forEach(moduleName -> addFailure("6.4.9.2.b - " + moduleName + " reported a different MIL status that it did for DM12 response earlier in this part"));
+
+        List<Integer> obdModuleAddresses = getDataRepository().getObdModuleAddresses();
+
         // 6.4.9.3.a DS DM27 to each OBD ECU.
+        var dsResults = obdModuleAddresses
+                .stream().map(address -> getDiagnosticMessageModule().requestDM27(getListener(), address))
+                .collect(Collectors.toList());
+
         // 6.4.9.4.a (if supported) Fail if any difference compared to data received from global request.
+        compareRequestPackets(globalPackets, filterPackets(dsResults), "6.4.9.4.a");
+
         // 6.4.9.4.b (if supported) Fail if NACK not received from OBD ECUs that did not respond to global query.
+        checkForNACKs(globalPackets, filterAcks(dsResults), obdModuleAddresses, "6.4.9.4.b");
     }
 
+    private DiagnosticTroubleCodePacket getDTCPacket(int moduleAddress) {
+        OBDModuleInformation obdModuleInformation = getDataRepository().getObdModule(moduleAddress);
+        return obdModuleInformation == null ? null : obdModuleInformation.get(DM12MILOnEmissionDTCPacket.class);
+    }
+
+    private LampStatus getLampStatus(int moduleAddress) {
+        var packet = getDTCPacket(moduleAddress);
+        return packet == null ? null : packet.getMalfunctionIndicatorLampStatus();
+    }
 }
