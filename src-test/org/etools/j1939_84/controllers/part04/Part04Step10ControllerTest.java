56685b8ea6fc5ff694d08628eeb6efd521b85c2e
Matt Gumbel <matt@soliddesign.net>
2021-02-19 08:44:45 -0500

    Implement Part 4 Step 10

--- src-test/org/etools/j1939_84/controllers/part04/Part04Step10ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part04/Part04Step10ControllerTest.java
@@ -3,15 +3,31 @@
  */
 package org.etools.j1939_84.controllers.part04;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM25ExpandedFreezeFrame;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
+import org.etools.j1939_84.bus.j1939.packets.FreezeFrame;
+import org.etools.j1939_84.bus.j1939.packets.model.Spn;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -118,11 +134,117 @@ public class Part04Step10ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation0 = new OBDModuleInformation(0);
+        obdModuleInformation0.set(DM12MILOnEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation0);
+
+        var spn = Spn.create(102, 900);
+        var dm25 = DM25ExpandedFreezeFrame.create(0, new FreezeFrame(dtc, spn));
+        when(diagnosticMessageModule.requestDM25(any(), eq(0))).thenReturn(new BusResult<>(false, dm25));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM25(any(), eq(1))).thenReturn(new BusResult<>(false, nack));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM25(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM25(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testNoMessages() {
 
         runTest();
 
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.4.10.2.a - No ECU reported freeze frame data");
     }
 
+    @Test
+    public void testFailureForNoFreezeFrameData() {
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM25(any(), eq(1))).thenReturn(new BusResult<>(false, nack));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM25(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.4.10.2.a - No ECU reported freeze frame data");
+    }
+
+    @Test
+    public void testFailureNoNACK() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation0 = new OBDModuleInformation(0);
+        obdModuleInformation0.set(DM12MILOnEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation0);
+
+        var spn = Spn.create(102, 900);
+        var dm25 = DM25ExpandedFreezeFrame.create(0, new FreezeFrame(dtc, spn));
+        when(diagnosticMessageModule.requestDM25(any(), eq(0))).thenReturn(new BusResult<>(false, dm25));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        when(diagnosticMessageModule.requestDM25(any(), eq(1))).thenReturn(new BusResult<>(true));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM25(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM25(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.4.10.2.c - OBD module Engine #2 (1) did not provide a NACK for the DS query");
+    }
+
+    @Test
+    public void testFailureForDifferentDTC() {
+        var dtc1 = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation0 = new OBDModuleInformation(0);
+        obdModuleInformation0.set(DM12MILOnEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc1));
+        dataRepository.putObdModule(obdModuleInformation0);
+
+        var spn = Spn.create(102, 900);
+        var dtc2 = DiagnosticTroubleCode.create(456, 12, 0, 1);
+
+        var dm25 = DM25ExpandedFreezeFrame.create(0, new FreezeFrame(dtc2, spn));
+        when(diagnosticMessageModule.requestDM25(any(), eq(0))).thenReturn(new BusResult<>(false, dm25));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM25(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.4.10.2.b - Engine #1 (0) did not report DTC in freeze frame data which included the DTC reported in DM12 earlier in this part");
+    }
 }
