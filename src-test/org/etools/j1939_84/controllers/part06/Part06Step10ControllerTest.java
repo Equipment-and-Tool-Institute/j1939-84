f087a215a79fc474fb481f3b122060cbc4aed85e
Matt Gumbel <matt@soliddesign.net>
2021-02-23 15:52:05 -0500

    Implement Part 6 Step 10

--- src-test/org/etools/j1939_84/controllers/part06/Part06Step10ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part06/Part06Step10ControllerTest.java
@@ -3,16 +3,32 @@
  */
 package org.etools.j1939_84.controllers.part06;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
+import static org.etools.j1939_84.model.Outcome.WARN;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM21DiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +135,195 @@ public class Part06Step10ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM21(any(), eq(1))).thenReturn(BusResult.of(nack));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM21(any(), eq(1));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testFailureForDistanceGreaterThanZero() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 1, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.6.10.2.a - Engine #1 (0) reported distance with MIL on > 0");
+    }
+
+    @Test
+    public void testFailureForDistanceNotSupported() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0xFFFF, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.6.10.2.a - Engine #1 (0) reported distance with MIL on is not supported");
+    }
+
+    @Test
+    public void testFailureForNoDTC() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.6.10.2.b - Engine #1 (0) reported with with MIL on > 0 minutes, and did not report a DTC in its DM12 response");
+    }
+
+    @Test
+    public void testFailureNoSupport() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var nack = AcknowledgmentPacket.create(0, NACK);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(nack));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.6.10.2.c - No ECU supports DM21");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        WARN,
+                                        "6.6.10.3.a - No ECU reported time with MIL on > 0 minutes");
+    }
+
+    @Test
+    public void testFailureForNoNACK() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        when(diagnosticMessageModule.requestDM21(any(), eq(1))).thenReturn(BusResult.empty());
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM21(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.6.10.2.d - OBD module Engine #2 (1) did not provide a NACK for the DS query");
+    }
+
+    @Test
+    public void testFailureForNoMILOnGreaterThanZero() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0, 0, 0, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        WARN,
+                                        "6.6.10.3.a - No ECU reported time with MIL on > 0 minutes");
+    }
+
+    @Test
+    public void testFailureForTimeDifference() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm21 = DM21DiagnosticReadinessPacket.create(0, 0, 0, 1, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(0))).thenReturn(BusResult.of(dm21));
+
+        var dtc_1 = DiagnosticTroubleCode.create(234, 12, 0, 1);
+
+        OBDModuleInformation obdModuleInformation1 = new OBDModuleInformation(1);
+        obdModuleInformation1.set(DM12MILOnEmissionDTCPacket.create(1, ON, OFF, OFF, OFF, dtc_1));
+        dataRepository.putObdModule(obdModuleInformation1);
+
+        var dm21_1 = DM21DiagnosticReadinessPacket.create(1, 0, 0, 3, 0);
+        when(diagnosticMessageModule.requestDM21(any(), eq(1))).thenReturn(BusResult.of(dm21_1));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM21(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM21(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        WARN,
+                                        "6.6.10.3.b - More than one ECU reported time with MIL on > 0 and difference between the times reported is > 1 minute");
     }
 
 }
--- src/org/etools/j1939_84/controllers/part06/Part06Step10Controller.java
+++ src/org/etools/j1939_84/controllers/part06/Part06Step10Controller.java
@@ -3,9 +3,15 @@
  */
 package org.etools.j1939_84.controllers.part06;
 
+import static org.etools.j1939_84.bus.j1939.packets.ParsedPacket.NOT_AVAILABLE;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM21DiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,14 +60,68 @@ public class Part06Step10Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.6.10.1.a. DS DM21 [(send Request (PGN 59904) for PGN 49408 (SPNs 3069, 3295)]) to each OBD ECU.
-        // 6.6.10.2.a. Fail if any ECU reports distance with MIL on (SPN 3069) is > 0 or reports not supported.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(a -> getDiagnosticMessageModule().requestDM21(getListener(), a))
+                                           .collect(Collectors.toList());
+
+        var packets = filterPackets(dsResults);
+
+        // 6.6.10.2.a. Fail if any ECU reports distance with MIL on (SPN 3069) is > 0.
+        packets.stream()
+               .filter(p -> p.getKmWhileMILIsActivated() > 0 && p.getKmWhileMILIsActivated() != NOT_AVAILABLE)
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.6.10.2.a - " + moduleName + " reported distance with MIL on > 0"));
+
+        // 6.6.10.2.a. Fail if any ECU reports distance with MIL on (SPN 3069) is not supported.
+        packets.stream()
+               .filter(p -> p.getKmWhileMILIsActivated() == NOT_AVAILABLE)
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.6.10.2.a - " + moduleName
+                       + " reported distance with MIL on is not supported"));
+
         // 6.6.10.2.b. Fail if any ECU reports time with MIL on greater than 0 minute, and did not report a DTC in its
         // DM12 response.
+        packets.stream()
+               .filter(p -> p.getMinutesWhileMILIsActivated() > 0)
+               .filter(p -> !hasDM12DTC(p.getSourceAddress()))
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.6.10.2.b - " + moduleName
+                       + " reported with with MIL on > 0 minutes, and did not report a DTC in its DM12 response"));
+
         // 6.6.10.2.c. Fail if no ECU supports DM21.
+        if (packets.isEmpty()) {
+            addFailure("6.6.10.2.c - No ECU supports DM21");
+        }
+
         // 6.6.10.2.d. Fail if NACK not received from OBD ECUs that did not provide a DM21 message.
+        checkForNACKsFromObdModules(packets, filterAcks(dsResults), "6.6.10.2.d");
+
         // 6.6.10.3.a. Warn if no ECU reports time with MIL on (SPN 3295) greater than 0 minute.
-        // 6.6.10.3.b. Warn if more than one ECU reports time with MIL on > 0 and difference between times reported is >
-        // 1 minute.
+        boolean noMilOnTime = packets.stream().noneMatch(p -> p.getMinutesWhileMILIsActivated() > 0);
+        if (noMilOnTime) {
+            addWarning("6.6.10.3.a - No ECU reported time with MIL on > 0 minutes");
+        }
+
+        // 6.6.10.3.b. Warn if more than one ECU reports time with MIL on > 0
+        // and difference between times reported is > 1 minute.
+        var min = packets.stream()
+                         .mapToDouble(DM21DiagnosticReadinessPacket::getMinutesWhileMILIsActivated)
+                         .filter(t -> t > 0)
+                         .min()
+                         .orElse(0.0);
+        var max = packets.stream()
+                         .mapToDouble(DM21DiagnosticReadinessPacket::getMinutesWhileMILIsActivated)
+                         .filter(t -> t > 0)
+                         .max()
+                         .orElse(0.0);
+        if (max - min > 1) {
+            addWarning("6.6.10.3.b - More than one ECU reported time with MIL on > 0 and difference between the times reported is > 1 minute");
+        }
+    }
+
+    private boolean hasDM12DTC(int address) {
+        return !getDTCs(DM12MILOnEmissionDTCPacket.class, address).isEmpty();
     }
 
 }
