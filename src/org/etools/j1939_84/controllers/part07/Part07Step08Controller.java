4165821dcd2130aa951d2b5328bacfda04fc8984
Matt Gumbel <matt@soliddesign.net>
2021-02-24 16:30:15 -0500

    Implement Part 7 Step 8

--- src/org/etools/j1939_84/controllers/part07/Part07Step08Controller.java
+++ src/org/etools/j1939_84/controllers/part07/Part07Step08Controller.java
@@ -3,9 +3,13 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,11 +58,33 @@ public class Part07Step08Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.7.8.1.a Global DM27 [(send Request (PGN 59904) for PGN 64898 (SPNs 1213-1215, 1706, and 3038)]).
+        var globalPackets = getDiagnosticMessageModule().requestDM27(getListener()).getPackets();
+
         // 6.7.8.2.a (if supported) Fail if any OBD ECU reports a pending DTC.
-        // 6.7.8.2.b (if supported)Fail if any ECU does not report MIL off.
+        globalPackets.stream()
+                     .filter(p -> isObdModule(p.getSourceAddress()))
+                     .filter(p -> !p.getDtcs().isEmpty())
+                     .map(ParsedPacket::getModuleName)
+                     .forEach(moduleName -> addFailure("6.7.8.2.a - " + moduleName + " reported a pending DTC"));
+
+        // 6.7.8.2.b (if supported) Fail if any ECU does not report MIL off.
+        globalPackets.stream()
+                     .filter(p -> isObdModule(p.getSourceAddress()))
+                     .filter(p -> p.getMalfunctionIndicatorLampStatus() != OFF)
+                     .map(ParsedPacket::getModuleName)
+                     .forEach(moduleName -> addFailure("6.7.8.2.a - " + moduleName + " did not report MIL off"));
+
         // 6.7.8.3.a DS DM27 to each OBD ECU.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(a -> getDiagnosticMessageModule().requestDM27(getListener(), a))
+                                           .collect(Collectors.toList());
+
         // 6.7.8.4.a (if supported) Fail if any difference compared to data received for global request.
+        compareRequestPackets(globalPackets, filterPackets(dsResults), "6.7.8.4.a");
+
         // 6.7.8.4.b (if supported) Fail if NACK not received from OBD ECUs that did not respond to global query
+        checkForNACKsGlobal(globalPackets, filterAcks(dsResults), "6.7.8.4.b");
     }
 
 }
