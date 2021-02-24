0a699944a182af42ac20a106e34e1e57a69a3892
Matt Gumbel <matt@soliddesign.net>
2021-02-24 16:20:46 -0500

    Implement Part 7 Step 7

--- src-test/org/etools/j1939_84/controllers/part07/Part07Step07ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part07/Part07Step07ControllerTest.java
@@ -3,16 +3,31 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ALTERNATE_OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM6PendingEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +134,107 @@ public class Part07Step07ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dm6 = DM6PendingEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM6(any(), eq(0))).thenReturn(RequestResult.of(dm6));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM6(any(), eq(1))).thenReturn(new RequestResult<>(false, nack));
+
+        when(diagnosticMessageModule.requestDM6(any())).thenReturn(RequestResult.of(dm6));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM6(any());
+        verify(diagnosticMessageModule).requestDM6(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM6(any(), eq(1));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testFailureForPendingDTC() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dtc = DiagnosticTroubleCode.create(123, 1, 0, 3);
+        var dm6 = DM6PendingEmissionDTCPacket.create(0, ALTERNATE_OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM6(any(), eq(0))).thenReturn(RequestResult.of(dm6));
+        when(diagnosticMessageModule.requestDM6(any())).thenReturn(RequestResult.of(dm6));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM6(any());
+        verify(diagnosticMessageModule).requestDM6(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.7.2.a - Engine #1 (0) reported a pending DTC");
+    }
+
+    @Test
+    public void testFailureForMILNotOff() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dm6 = DM6PendingEmissionDTCPacket.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM6(any(), eq(0))).thenReturn(RequestResult.of(dm6));
+        when(diagnosticMessageModule.requestDM6(any())).thenReturn(RequestResult.of(dm6));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM6(any());
+        verify(diagnosticMessageModule).requestDM6(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.7.2.a - Engine #1 (0) did not report MIL 'off'");
+    }
+
+    @Test
+    public void testFailureForDifference() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dm6_0 = DM6PendingEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM6(any())).thenReturn(RequestResult.of(dm6_0));
+
+        var dm6_1 = DM6PendingEmissionDTCPacket.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM6(any(), eq(0))).thenReturn(RequestResult.of(dm6_1));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM6(any());
+        verify(diagnosticMessageModule).requestDM6(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.7.4.a - Difference compared to data received during global request from Engine #1 (0)");
+    }
+
+    @Test
+    public void testFailureForNoNACK() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        when(diagnosticMessageModule.requestDM6(any())).thenReturn(RequestResult.empty());
+        when(diagnosticMessageModule.requestDM6(any(), eq(0))).thenReturn(RequestResult.empty());
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM6(any());
+        verify(diagnosticMessageModule).requestDM6(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.7.4.b - OBD module Engine #1 (0) did not provide a response to Global query and did not provide a NACK for the DS query");
     }
 
 }
