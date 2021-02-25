0d78ccd5b11753e417e436187b4f55ba2ca07222
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-25 08:46:12 -0500

    Implement Part 7 Step 5 (#630)

--- src/org/etools/j1939_84/controllers/part07/Part07Step05Controller.java
+++ src/org/etools/j1939_84/controllers/part07/Part07Step05Controller.java
@@ -3,9 +3,13 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,9 +58,27 @@ public class Part07Step05Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.7.5.1.a Receive broadcast data [(PGN 65226 (SPNs 1213-1215, 1706, and 3038)]).
+        var packets = getDiagnosticMessageModule().readDM1(getListener())
+                                                  .stream()
+                                                  .filter(p -> isObdModule(p.getSourceAddress()))
+                                                  .collect(Collectors.toList());
+
         // 6.7.5.2.a Fail if any OBD ECU reports an active DTC.
+        packets.stream()
+               .filter(p -> !p.getDtcs().isEmpty())
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.7.5.2.a - " + moduleName + " reported an active DTC"));
+
         // 6.7.5.2.b Fail if any OBD ECU does not report MIL off.
+        packets.stream()
+               .filter(p -> p.getMalfunctionIndicatorLampStatus() != OFF)
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.7.5.2.a - " + moduleName + " did not report MIL off"));
+
         // 6.7.5.2.c Fail if no OBD ECU provides DM1.
+        if (packets.isEmpty()) {
+            addFailure("6.7.5.2.c - No OBD ECU provided a DM1");
+        }
     }
 
 }
