d6ff2eeeaa7889ae5e4eb6892c8643274f91add0
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-25 10:08:51 -0500

    Implement Part 7 Step 13 (#640)

--- src-test/org/etools/j1939_84/controllers/part07/Part07Step13ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part07/Part07Step13ControllerTest.java
@@ -3,16 +3,25 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +128,62 @@ public class Part07Step13ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.setIgnitionCycleCounterValue(9);
+        dataRepository.putObdModule(obdModuleInformation);
+        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 12, 5);
+        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
+
+        // Won't be asked for DM20
+        dataRepository.putObdModule(new OBDModuleInformation(1));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testFailureForTooMany() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.setIgnitionCycleCounterValue(9);
+        dataRepository.putObdModule(obdModuleInformation);
+        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 14, 5);
+        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.13.2.a - Ignition cycle counter for Engine #1 (0) has incremented by other than 3 cycles from part 5");
+    }
+
+    @Test
+    public void testFailureForTooFew() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.setIgnitionCycleCounterValue(9);
+        dataRepository.putObdModule(obdModuleInformation);
+        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 9, 5);
+        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.13.2.a - Ignition cycle counter for Engine #1 (0) has incremented by other than 3 cycles from part 5");
     }
 
 }
