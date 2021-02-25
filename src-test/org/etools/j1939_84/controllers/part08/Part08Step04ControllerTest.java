99997207b65fa83580b1ca40b3a5cacafea9b82a
Matt Gumbel <matt@soliddesign.net>
2021-02-25 15:59:18 -0500

    Implement Part 8 Step 4

--- src-test/org/etools/j1939_84/controllers/part08/Part08Step04ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part08/Part08Step04ControllerTest.java
@@ -3,16 +3,29 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.FAST_FLASH;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +132,92 @@ public class Part08Step04ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc = DiagnosticTroubleCode.create(123, 1, 1, 2);
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm23_0 = DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc);
+        var dm23_1 = DM23PreviouslyMILOnEmissionDTCPacket.create(1, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM23(any())).thenReturn(RequestResult.of(dm23_0, dm23_1));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM23(any());
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testFailureForNoDTC() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm23 = DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM23(any())).thenReturn(RequestResult.of(dm23));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM23(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.8.4.2.a - No OBD ECU reported a previously active DTC");
+    }
+
+    @Test
+    public void testFailureForDifferentDTCs() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc1 = DiagnosticTroubleCode.create(123, 1, 1, 2);
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc1));
+        var dtc2 = DiagnosticTroubleCode.create(2342, 1, 1, 2);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc2));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm23 = DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc2);
+        when(diagnosticMessageModule.requestDM23(any())).thenReturn(RequestResult.of(dm23));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM23(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.8.4.2.b - Previously active DTC reported by Engine #1 (0) is not the same as previously active DTC from part 7");
+    }
+
+    @Test
+    public void testFailureForDifferentMIL() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc = DiagnosticTroubleCode.create(123, 1, 1, 2);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, FAST_FLASH, OFF, OFF, OFF, dtc));
+
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm23 = DM23PreviouslyMILOnEmissionDTCPacket.create(0, FAST_FLASH, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM23(any())).thenReturn(RequestResult.of(dm23));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM23(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.8.4.2.c - Engine #1 (0) reported different MIL status than DM12 response earlier in this part");
     }
 
 }
