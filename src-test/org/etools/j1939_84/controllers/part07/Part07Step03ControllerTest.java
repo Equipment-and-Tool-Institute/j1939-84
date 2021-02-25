dec4271d40cdd8ab95d1fb70ea3f898703173888
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-25 08:45:45 -0500

    Implement Part 7 Step 3 (#628)
    * Implement Part 7 Step 3
    
    * save the DM2

--- src-test/org/etools/j1939_84/controllers/part07/Part07Step03ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part07/Part07Step03ControllerTest.java
@@ -3,16 +3,34 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.NOT_SUPPORTED;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM2PreviouslyActiveDTC;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +137,248 @@ public class Part07Step03ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(BusResult.of(nack));
+
+        var dm2_2 = DM2PreviouslyActiveDTC.create(2, NOT_SUPPORTED, NOT_SUPPORTED, NOT_SUPPORTED, NOT_SUPPORTED);
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0, dm2_2));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testNoPackets() {
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.empty());
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.a - No OBD ECU reported previously active DTC(s)");
+    }
+
+    @Test
+    public void testFailureForNoDTCs() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        var dm2_2 = DM2PreviouslyActiveDTC.create(2, OFF, OFF, OFF, OFF, dtc);
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0, dm2_2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.a - No OBD ECU reported previously active DTC(s)");
+    }
+
+    @Test
+    public void testFailureForDifferentSizeDTCs() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+        var dtc2 = DiagnosticTroubleCode.create(423, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc, dtc2));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.b - Engine #1 (0) reported fewer previously active DTCs than in DM23 response earlier in this part");
+    }
+
+    @Test
+    public void testFailureForDifferentDTCInDM12() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dtc2 = DiagnosticTroubleCode.create(423, 11, 1, 19);
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc2);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.c - Engine #1 (0) DM2 response does not include SPN = 123, FMI = 11 in the previous DM12 response");
+    }
+
+    @Test
+    public void testFailureForOBDMilNotOff() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.d - Engine #1 (0) did not report MIL 'off'");
+
+    }
+
+    @Test
+    public void testFailureForNonOBDMILNotOff() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        var dm2_2 = DM2PreviouslyActiveDTC.create(2, ON, OFF, OFF, OFF);
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0, dm2_2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.2.e - Turbocharger (2) did not report MIL off or not supported");
+
+    }
+
+    @Test
+    public void testFailureForDifference() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM12MILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        obdModuleInformation.set(DM23PreviouslyMILOnEmissionDTCPacket.create(0, ON, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        var dm2_0_2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0_2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.4.a - Difference compared to data received during global request from Engine #1 (0)");
+
+    }
+
+    @Test
+    public void testFailureForNoNack() {
+        var dtc = DiagnosticTroubleCode.create(123, 11, 1, 19);
+
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm2_0 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2_0));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(BusResult.empty());
+
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(RequestResult.of(dm2_0));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.7.3.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
     }
 
 }
--- src/org/etools/j1939_84/controllers/part07/Part07Step03Controller.java
+++ src/org/etools/j1939_84/controllers/part07/Part07Step03Controller.java
@@ -3,9 +3,19 @@
  */
 package org.etools.j1939_84.controllers.part07;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ALTERNATE_OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.NOT_SUPPORTED;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
+import org.etools.j1939_84.bus.j1939.packets.LampStatus;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,16 +64,79 @@ public class Part07Step03Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.7.3.1.a Global DM2 [(send Request (PGN 59904) for PGN 65227 (SPNs 1213-1215, 1706, and 3038)]).
+        var globalPackets = getDiagnosticMessageModule().requestDM2(getListener()).getPackets();
+
         // 6.7.3.2.a (if supported) Fail if no OBD ECU reports any previously active DTC(s).
+        boolean noDTCs = globalPackets.stream()
+                                      .filter(p -> isObdModule(p.getSourceAddress()))
+                                      .allMatch(p -> p.getDtcs().isEmpty());
+        if (noDTCs) {
+            addFailure("6.7.3.2.a - No OBD ECU reported previously active DTC(s)");
+        }
+
+        // Save the responses from each OBD Module
+        globalPackets.forEach(this::save);
+
         // 6.7.3.2.b (if supported) Fail if any OBD ECU reports a fewer previously active DTCs than in DM23 response
         // earlier in this part.
-        // 6.7.3.2.c (if supported) Fail if any OBD ECU fails to provide its DTC from its DM12 response in part 6 as a
-        // previously active DTC in its DM2 response.
+        globalPackets.stream()
+                     .filter(p -> isObdModule(p.getSourceAddress()))
+                     .filter(p -> p.getDtcs()
+                                   .size() < getDTCs(DM23PreviouslyMILOnEmissionDTCPacket.class,
+                                                     p.getSourceAddress()).size())
+                     .map(ParsedPacket::getModuleName)
+                     .forEach(moduleName -> addFailure("6.7.3.2.b - " + moduleName
+                             + " reported fewer previously active DTCs than in DM23 response earlier in this part"));
+
+        // 6.7.3.2.c (if supported) Fail if any OBD ECU fails to provide its DTC from its DM12 response in part 6
+        // as a previously active DTC in its DM2 response.
+        globalPackets.stream()
+                     .filter(p -> isObdModule(p.getSourceAddress()))
+                     .forEach(p -> {
+                         var dm2DTCs = p.getDtcs();
+                         for (DiagnosticTroubleCode dtc : getDTCs(DM12MILOnEmissionDTCPacket.class,
+                                                                  p.getSourceAddress())) {
+                             if (!listContainsDTC(dm2DTCs, dtc)) {
+                                 int spn = dtc.getSuspectParameterNumber();
+                                 int fmi = dtc.getFailureModeIndicator();
+                                 addFailure("6.7.3.2.c - " + p.getModuleName() + " DM2 response does not include SPN = "
+                                         + spn + ", FMI = " + fmi + " in the previous DM12 response");
+                             }
+                         }
+                     });
+
         // 6.7.3.2.d (if supported) Fail if any OBD ECU does not report MIL off. See Section A.8 for allowed values.
-        // Fail if any non-OBD ECU does not report MIL off or not supported.
+        globalPackets.stream()
+                     .filter(p -> isObdModule(p.getSourceAddress()))
+                     .filter(p -> {
+                         LampStatus mil = p.getMalfunctionIndicatorLampStatus();
+                         return mil != OFF && mil != ALTERNATE_OFF;
+                     })
+                     .map(ParsedPacket::getModuleName)
+                     .forEach(moduleName -> addFailure("6.7.3.2.d - " + moduleName + " did not report MIL 'off'"));
+
+        // 6.7.3.2.e Fail if any non-OBD ECU does not report MIL off or not supported.
+        globalPackets.stream()
+                     .filter(p -> !isObdModule(p.getSourceAddress()))
+                     .filter(p -> {
+                         LampStatus mil = p.getMalfunctionIndicatorLampStatus();
+                         return mil != OFF && mil != NOT_SUPPORTED;
+                     })
+                     .map(ParsedPacket::getModuleName)
+                     .forEach(moduleName -> addFailure("6.7.3.2.e - " + moduleName
+                             + " did not report MIL off or not supported"));
+
         // 6.7.3.3.a DS DM2 to each OBD ECU.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(a -> getDiagnosticMessageModule().requestDM2(getListener(), a))
+                                           .collect(Collectors.toList());
+
         // 6.7.3.4.a (if supported) Fail if any difference compared to data received for global request.
+        compareRequestPackets(globalPackets, filterPackets(dsResults), "6.7.3.4.a");
+
         // 6.7.3.4.b (if supported) Fail if NACK not received from OBD ECUs that did not respond to global query.
+        checkForNACKsGlobal(globalPackets, filterAcks(dsResults), "6.7.3.4.b");
     }
 
 }
