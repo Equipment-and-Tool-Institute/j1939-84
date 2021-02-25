5744a9e9c9191fbdf6a813cb9d518448aded5457
Matt Gumbel <matt@soliddesign.net>
2021-02-25 15:37:26 -0500

    Implement Part 8 Step 1

--- src-test/org/etools/j1939_84/controllers/part08/Part08Step01ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part08/Part08Step01ControllerTest.java
@@ -3,9 +3,17 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import static org.etools.j1939_84.J1939_84.NL;
+import static org.etools.j1939_84.controllers.ResultsListener.MessageType.WARNING;
+import static org.etools.j1939_84.model.Outcome.ABORT;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Executor;
 
 import org.etools.j1939_84.bus.j1939.J1939;
@@ -18,7 +26,6 @@ import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.ReportFileModule;
-import org.etools.j1939_84.modules.TestDateTimeModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
 import org.etools.j1939_84.utils.AbstractControllerTest;
 import org.junit.After;
@@ -59,19 +66,17 @@ public class Part08Step01ControllerTest extends AbstractControllerTest {
 
     private TestResultsListener listener;
 
-    private DataRepository dataRepository;
-
     private StepController instance;
 
     @Before
     public void setUp() throws Exception {
-        dataRepository = DataRepository.newInstance();
         listener = new TestResultsListener(mockListener);
+        DateTimeModule.setInstance(null);
 
         instance = new Part08Step01Controller(executor,
                                               bannerModule,
-                                              new TestDateTimeModule(),
-                                              dataRepository,
+                                              DateTimeModule.getInstance(),
+                                              DataRepository.newInstance(),
                                               engineSpeedModule,
                                               vehicleInformationModule,
                                               diagnosticMessageModule);
@@ -118,12 +123,91 @@ public class Part08Step01ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testHappyPathNoFailures() {
+    public void testRun() {
+        when(engineSpeedModule.isEngineRunning()).thenReturn(true);
+        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("0.0 RPMs");
 
         runTest();
 
-        assertEquals("", listener.getMessages());
-        assertEquals("", listener.getResults());
+        verify(engineSpeedModule, atLeastOnce()).getEngineSpeedAsString();
+        verify(engineSpeedModule, atLeastOnce()).isEngineRunning();
+
+        String expectedMessages = "";
+        assertEquals(expectedMessages, listener.getMessages());
+
+        String expectedMilestones = "";
+        assertEquals(expectedMilestones, listener.getMilestones());
+
+        String expectedResults = "";
+        expectedResults += "Initial Engine Speed = 0.0 RPMs" + NL;
+        expectedResults += "Final Engine Speed = 0.0 RPMs" + NL;
+        assertEquals(expectedResults, listener.getResults());
+    }
+
+    @Test
+    public void testWaitForKeyOn() {
+        when(engineSpeedModule.isEngineRunning()).thenReturn(false);
+        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("148.6 RPMs");
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                when(engineSpeedModule.isEngineRunning()).thenReturn(true);
+            }
+        }, 750);
+
+        runTest();
+
+        verify(engineSpeedModule, atLeastOnce()).isEngineRunning();
+        verify(engineSpeedModule, atLeastOnce()).getEngineSpeedAsString();
+
+        verify(mockListener).onUrgentMessage("Please turn the Key ON with Engine ON", "Adjust Key Switch", WARNING);
+
+        String expectedMessages = "Waiting for Key ON, Engine ON..." + NL;
+        expectedMessages += "Waiting for Key ON, Engine ON...";
+        assertEquals(expectedMessages, listener.getMessages());
+
+        String expectedMilestones = "";
+        assertEquals(expectedMilestones, listener.getMilestones());
+
+        String expectedResults = "Initial Engine Speed = 148.6 RPMs" + NL;
+        expectedResults += "Final Engine Speed = 148.6 RPMs" + NL;
+        assertEquals(expectedResults, listener.getResults());
+    }
+
+    @Test
+    public void testEngineThrowInterruptedException() {
+
+        when(engineSpeedModule.isEngineRunning()).thenReturn(false);
+        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("300.0 RPMs");
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                instance.stop();
+            }
+        }, 750);
+
+        runTest();
+
+        verify(engineSpeedModule).getEngineSpeedAsString();
+        verify(engineSpeedModule, atLeastOnce()).isEngineRunning();
+
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, ABORT, "User cancelled testing at Part 8 Step 1");
+        verify(mockListener).onUrgentMessage("Please turn the Key ON with Engine ON", "Adjust Key Switch", WARNING);
+
+        String expectedMessages = "Waiting for Key ON, Engine ON..." + NL;
+        expectedMessages += "Waiting for Key ON, Engine ON..." + NL;
+        expectedMessages += "Waiting for Key ON, Engine ON..." + NL;
+        expectedMessages += "User cancelled testing at Part 8 Step 1";
+        assertEquals(expectedMessages, listener.getMessages());
+
+        String expectedMilestones = "";
+        assertEquals(expectedMilestones, listener.getMilestones());
+
+        String expectedResults = "";
+        expectedResults += "Initial Engine Speed = 300.0 RPMs" + NL;
+        assertEquals(expectedResults, listener.getResults());
     }
 
 }
