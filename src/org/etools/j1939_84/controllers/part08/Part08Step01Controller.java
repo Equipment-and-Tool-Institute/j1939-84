1314f243d09579f59db501af5c9f2a1985212afa
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-26 11:08:16 -0500

    Implement Part 8 Step 1 (#647)

--- src/org/etools/j1939_84/controllers/part08/Part08Step01Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step01Controller.java
@@ -56,6 +56,7 @@ public class Part08Step01Controller extends StepController {
         // 6.8.1.1.a Gather broadcast data for engine speed (e.g., SPN 190)
         // 6.8.1.2.a If after 5 seconds engine speed is < 400 rpm, prompt/warn operator to confirm engine is running and
         // then press enter.
+        ensureKeyOnEngineOn();
     }
 
 }
