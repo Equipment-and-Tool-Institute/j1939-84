d6f753cf48ad8e7642ab80f46cc1e1a0b4d78534
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-26 12:07:38 -0500

    Implement Part 8 Step 2 (#645)
    * Implement Part 8 Step 2
    
    * Update message

--- src/org/etools/j1939_84/controllers/part04/Part04Step02Controller.java
+++ src/org/etools/j1939_84/controllers/part04/Part04Step02Controller.java
@@ -5,12 +5,14 @@ package org.etools.j1939_84.controllers.part04;
 
 import static org.etools.j1939_84.J1939_84.NL;
 import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.controllers.QuestionListener.AnswerType.CANCEL;
+import static org.etools.j1939_84.controllers.QuestionListener.AnswerType.NO;
 import static org.etools.j1939_84.controllers.ResultsListener.MessageType.QUESTION;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 import org.etools.j1939_84.bus.j1939.BusResult;
@@ -22,9 +24,8 @@ import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCodePacket;
 import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
+import org.etools.j1939_84.controllers.QuestionListener;
 import org.etools.j1939_84.controllers.StepController;
-import org.etools.j1939_84.model.OBDModuleInformation;
-import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -70,11 +71,10 @@ public class Part04Step02Controller extends StepController {
 
     @Override
     protected void run() throws Throwable {
-
         int attempts = 0;
         List<DM12MILOnEmissionDTCPacket> globalPackets = List.of();
-        boolean foundDTC = false;
-        while (!foundDTC) {
+        AtomicBoolean foundDTC = new AtomicBoolean(false);
+        while (!foundDTC.get()) {
             // 6.4.2.1.a. Global DM12 ([send Request (PGN 59904) for PGN 65236 (SPN 1213-1215, 1706, and 3038)])
             // to retrieve confirmed and active DTCs.
 
@@ -82,28 +82,35 @@ public class Part04Step02Controller extends StepController {
             // reports a confirmed and active DTC.
 
             attempts++;
-            updateProgress("Step 4.2. Requesting DM12 Attempt " + attempts);
+            updateProgress("Step 6.4.2.1.a Requesting DM12 Attempt " + attempts);
 
             getListener().onResult(NL + "Attempt " + attempts);
             globalPackets = getDiagnosticMessageModule().requestDM12(getListener()).getPackets();
 
-            foundDTC = globalPackets.stream()
-                                    .map(DiagnosticTroubleCodePacket::getDtcs)
-                                    .map(dtcs -> !dtcs.isEmpty())
-                                    .findFirst()
-                                    .orElse(false);
+            foundDTC.set(globalPackets.stream()
+                                      .map(DiagnosticTroubleCodePacket::getDtcs)
+                                      .map(dtcs -> !dtcs.isEmpty())
+                                      .findFirst()
+                                      .orElse(false));
 
-            if (!foundDTC) {
+            if (!foundDTC.get()) {
                 if (attempts == 5 * 60) {
                     // 6.4.2.1.a.ii. Time-out after every 5 minutes
                     // and ask user “‘yes/no”’ to continue if there is still no confirmed and active DTC;
                     // fail if user says “'no”' and no ECU reports a confirmed and active DTC.
 
-                    // This will throw an exception if the user chooses 'no'
-                    displayInstructionAndWait("No module has reported a confirmed and active DTC." + NL +
-                            "Do you wish to continue?",
-                                              "No Confirmed and Active DTCs Found",
-                                              QUESTION);
+                    QuestionListener questionListener = answer -> {
+                        if (answer == CANCEL || answer == NO) {
+                            addFailure("6.4.2.1.a.ii - User said 'no' and no ECU reported a confirmed and active DTC");
+                            foundDTC.set(true);
+                        }
+                    };
+
+                    String msg = "No module has reported a confirmed and active DTC." + NL + "Do you wish to continue?";
+                    getListener().onUrgentMessage(msg,
+                                                  "No Confirmed and Active DTCs Found",
+                                                  QUESTION,
+                                                  questionListener);
                     attempts = 0;
                 } else {
                     getDateTimeModule().pauseFor(1000);
@@ -112,14 +119,7 @@ public class Part04Step02Controller extends StepController {
         }
 
         // Save the DTCs per module
-        globalPackets.stream()
-                     .filter(p -> getDataRepository().isObdModule(p.getSourceAddress()))
-                     .filter(p -> !p.getDtcs().isEmpty())
-                     .forEach(p -> {
-                         var moduleInfo = getDataRepository().getObdModule(p.getSourceAddress());
-                         moduleInfo.set(p);
-                         getDataRepository().putObdModule(moduleInfo);
-                     });
+        globalPackets.forEach(this::save);
 
         // 6.4.2.2.a. Fail if no ECU reports MIL on. See Section A.8 for allowed values.
         boolean isMILOn = globalPackets.stream().anyMatch(p -> p.getMalfunctionIndicatorLampStatus() == ON);
@@ -151,14 +151,13 @@ public class Part04Step02Controller extends StepController {
             addWarning("6.4.2.3.b - More than one ECU reported a confirmed and active DTC");
         }
 
-        List<Integer> obdModuleAddresses = getDataRepository().getObdModuleAddresses();
-
         // 6.4.2.4.a. DS DM12 to each OBD ECU.
-        List<RequestResult<DM12MILOnEmissionDTCPacket>> dsResults = obdModuleAddresses.stream()
-                                                                                      .map(address -> getDiagnosticMessageModule().requestDM12(getListener(),
-                                                                                                                                               address))
-                                                                                      .map(BusResult::requestResult)
-                                                                                      .collect(Collectors.toList());
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(address -> getDiagnosticMessageModule().requestDM12(getListener(),
+                                                                                                    address))
+                                           .map(BusResult::requestResult)
+                                           .collect(Collectors.toList());
 
         // 6.4.2.5.a. Fail if any difference compared to data received from global request.
         List<DM12MILOnEmissionDTCPacket> dsPackets = filterRequestResultPackets(dsResults);
@@ -170,17 +169,7 @@ public class Part04Step02Controller extends StepController {
     }
 
     private List<DiagnosticTroubleCode> getDTCs(int moduleAddress) {
-        List<DiagnosticTroubleCode> results = new ArrayList<>();
-
-        OBDModuleInformation obdModuleInformation = getDataRepository().getObdModule(moduleAddress);
-        if (obdModuleInformation != null) {
-            DM6PendingEmissionDTCPacket packet = obdModuleInformation.get(DM6PendingEmissionDTCPacket.class);
-            if (packet != null) {
-                results.addAll(packet.getDtcs());
-            }
-        }
-
-        return results;
+        return getDTCs(DM6PendingEmissionDTCPacket.class, moduleAddress);
     }
 
 }
