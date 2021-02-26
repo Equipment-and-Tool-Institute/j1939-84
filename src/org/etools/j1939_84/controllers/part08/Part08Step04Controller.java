3aa627c18e97cad7cf1310423b769970860ccdb5
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-02-26 11:08:29 -0500

    Implement Part 8 Step 4 (#648)

--- src/org/etools/j1939_84/controllers/part08/Part08Step04Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step04Controller.java
@@ -3,9 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM12MILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM23PreviouslyMILOnEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
+import org.etools.j1939_84.bus.j1939.packets.LampStatus;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,9 +61,41 @@ public class Part08Step04Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.8.4.1.a Global DM23 [(send Request (PGN 59904) for PGN 64949 (SPNs 1213-1215, 1706, and 3038)]).
+        var packets = getDiagnosticMessageModule().requestDM23(getListener())
+                                                  .getPackets()
+                                                  .stream()
+                                                  .filter(p -> isObdModule(p.getSourceAddress()))
+                                                  .collect(Collectors.toList());
+
         // 6.8.4.2.a Fail if no OBD ECU reports a previously active DTC.
+        boolean noDTCs = packets.stream().allMatch(p -> p.getDtcs().isEmpty());
+        if (noDTCs) {
+            addFailure("6.8.4.2.a - No OBD ECU reported a previously active DTC");
+        }
+
         // 6.8.4.2.b Fail if previously active DTC reported is not the same as previously active DTC from part 7.
+        packets.stream()
+               .filter(p -> !dtcListsAreSame(p.getDtcs(), getDTCs(p.getSourceAddress())))
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.8.4.2.b - Previously active DTC reported by " + moduleName
+                       + " is not the same as previously active DTC from part 7"));
+
         // 6.8.4.2.c Fail if any ECU reporting different MIL status than DM12 response earlier in this part.
+        packets.stream()
+               .filter(p -> getMIL(p.getSourceAddress()) != null)
+               .filter(p -> p.getMalfunctionIndicatorLampStatus() != getMIL(p.getSourceAddress()))
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> addFailure("6.8.4.2.c - " + moduleName
+                       + " reported different MIL status than DM12 response earlier in this part"));
+    }
+
+    private List<DiagnosticTroubleCode> getDTCs(int address) {
+        return getDTCs(DM23PreviouslyMILOnEmissionDTCPacket.class, address);
+    }
+
+    private LampStatus getMIL(int address) {
+        var dm12 = get(DM12MILOnEmissionDTCPacket.class, address);
+        return dm12 == null ? null : dm12.getMalfunctionIndicatorLampStatus();
     }
 
 }
