e3fd6a8a48bd234d3d1ea48e1a7eda98f7808767
Matt Gumbel <matt@soliddesign.net>
2021-03-02 15:46:46 -0500

    Implement Part 8 Step 13

--- src/org/etools/j1939_84/controllers/part08/Part08Step13Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step13Controller.java
@@ -3,11 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
+import org.etools.j1939_84.controllers.part01.SectionA5Verifier;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -22,6 +27,8 @@ public class Part08Step13Controller extends StepController {
     private static final int STEP_NUMBER = 13;
     private static final int TOTAL_STEPS = 0;
 
+    private final SectionA5Verifier verifier;
+
     Part08Step13Controller() {
         this(Executors.newSingleThreadScheduledExecutor(),
              new BannerModule(),
@@ -29,7 +36,8 @@ public class Part08Step13Controller extends StepController {
              DataRepository.getInstance(),
              new EngineSpeedModule(),
              new VehicleInformationModule(),
-             new DiagnosticMessageModule());
+             new DiagnosticMessageModule(),
+             new SectionA5Verifier(PART_NUMBER, STEP_NUMBER));
     }
 
     Part08Step13Controller(Executor executor,
@@ -38,7 +46,8 @@ public class Part08Step13Controller extends StepController {
                            DataRepository dataRepository,
                            EngineSpeedModule engineSpeedModule,
                            VehicleInformationModule vehicleInformationModule,
-                           DiagnosticMessageModule diagnosticMessageModule) {
+                           DiagnosticMessageModule diagnosticMessageModule,
+                           SectionA5Verifier verifier) {
         super(executor,
               bannerModule,
               dateTimeModule,
@@ -49,16 +58,38 @@ public class Part08Step13Controller extends StepController {
               PART_NUMBER,
               STEP_NUMBER,
               TOTAL_STEPS);
+        this.verifier = verifier;
     }
 
     @Override
     protected void run() throws Throwable {
         // 6.8.13.1.a. DS DM3 [(send Request (PGN 59904) for PGN 65228]) to each OBD ECU.
+        for (OBDModuleInformation obdInfo : getDataRepository().getObdModules()) {
+
+            // 6.8.13.2.a. Fail if any ECU does not NACK
+            boolean nacked = getDiagnosticMessageModule().requestDM3(getListener(), obdInfo.getSourceAddress())
+                                                         .stream()
+                                                         .map(AcknowledgmentPacket::getResponse)
+                                                         .anyMatch(r -> r == NACK);
+            if (!nacked) {
+                addFailure("6.8.13.2.a - " + obdInfo.getModuleName() + " did not NACK");
+            }
+        }
+
         // 6.8.13.1.b. Wait 5 seconds before checking for erased information.
-        // 6.8.13.2.a. Fail if any ECU does not NACK or if any diagnostic information erased.
+        pause("Step 6.8.13.1.b. Waiting %1$d seconds before checking for erased information", 5);
+
+        // 6.8.13.2.a. Fail if any diagnostic information erased.
+        verifier.verifyDataNotErased(getListener(), "6.8.13.2.a");
+
         // 6.8.13.3.a. Global DM3.
+        getDiagnosticMessageModule().requestDM3(getListener());
+
         // 6.8.13.3.b. Wait 5 seconds before checking for erased information.
+        pause("Step 6.8.13.3.b. Waiting %1$d seconds before checking for erased information", 5);
+
         // 6.8.13.4.a. Fail if any OBD ECU erases any diagnostic information.
+        verifier.verifyDataNotErased(getListener(), "6.8.13.4.a");
     }
 
 }
