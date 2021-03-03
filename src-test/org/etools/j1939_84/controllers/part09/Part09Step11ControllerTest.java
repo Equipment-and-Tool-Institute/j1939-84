99642bc59cc5c54dfc37c95537bbc5b08b86f3fe
Matt Gumbel <matt@soliddesign.net>
2021-03-03 11:22:49 -0500

    Revert "Implement Part 9 Step 11"
    This reverts commit 0ee03fbd179e27cecdff39a6aa39eec09093403d.

--- src-test/org/etools/j1939_84/controllers/part09/Part09Step11ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part09/Part09Step11ControllerTest.java
@@ -3,27 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
-import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.concurrent.Executor;
 
-import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
-import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
-import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
-import org.etools.j1939_84.bus.j1939.packets.PerformanceRatio;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
-import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -130,121 +119,11 @@ public class Part09Step11ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
-        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
-        var ratio1 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio2 = new PerformanceRatio(456, 8, 100, 0);
-        obdModuleInformation.set(DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio1, ratio2));
-        dataRepository.putObdModule(obdModuleInformation);
-
-        var ratio11 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio21 = new PerformanceRatio(456, 8, 100, 0);
-        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio11, ratio21);
-        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
-
-        dataRepository.putObdModule(new OBDModuleInformation(1));
 
         runTest();
 
-        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
-
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
-        assertEquals(List.of(), listener.getOutcomes());
-    }
-
-    @Test
-    public void testFailureForDifferentIgnitionCycles() {
-        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
-        var ratio1 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio2 = new PerformanceRatio(456, 8, 100, 0);
-        obdModuleInformation.set(DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio1, ratio2));
-        dataRepository.putObdModule(obdModuleInformation);
-
-        var ratio11 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio21 = new PerformanceRatio(456, 8, 100, 0);
-        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 8, 2, ratio11, ratio21);
-        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
-
-        assertEquals("", listener.getMessages());
-        assertEquals("", listener.getResults());
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.11.2.a - Engine #1 (0) reported value for ignition cycles is not equal to the value from Step 6.9.4.1.b");
-    }
-
-    @Test
-    public void testFailureForDifferentRatios() {
-        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
-        var ratio1 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio2 = new PerformanceRatio(456, 8, 100, 0);
-        obdModuleInformation.set(DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio1, ratio2));
-        dataRepository.putObdModule(obdModuleInformation);
-
-        var ratio11 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio21 = new PerformanceRatio(789, 8, 100, 0);
-        var dm20 = DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio11, ratio21);
-        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(dm20));
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
-
-        assertEquals("", listener.getMessages());
-        assertEquals("", listener.getResults());
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.11.2.a - Engine #1 (0) reported values for performance ratios not equal to the values from Step 6.9.4.1.b");
-    }
-
-    @Test
-    public void testFailureForNACKNow() {
-        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
-        var ratio1 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio2 = new PerformanceRatio(456, 8, 100, 0);
-        obdModuleInformation.set(DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio1, ratio2));
-        dataRepository.putObdModule(obdModuleInformation);
-
-        var nack = AcknowledgmentPacket.create(0, AcknowledgmentPacket.Response.NACK);
-        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.of(nack));
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
-
-        assertEquals("", listener.getMessages());
-        assertEquals("", listener.getResults());
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.11.2.b - Engine #1 (0) now NACK'd DM20 request after previously providing data in 6.9.4.1");
-    }
-
-    @Test
-    public void testFailureForNoNACK() {
-        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
-        var ratio1 = new PerformanceRatio(123, 1, 10, 0);
-        var ratio2 = new PerformanceRatio(456, 8, 100, 0);
-        obdModuleInformation.set(DM20MonitorPerformanceRatioPacket.create(0, 4, 2, ratio1, ratio2));
-        dataRepository.putObdModule(obdModuleInformation);
-
-        when(diagnosticMessageModule.requestDM20(any(), eq(0))).thenReturn(BusResult.empty());
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM20(any(), eq(0));
-
-        assertEquals("", listener.getMessages());
-        assertEquals("", listener.getResults());
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.11.2.c - OBD module Engine #1 (0) did not provide a NACK for the DS query");
     }
 
 }
--- src/org/etools/j1939_84/controllers/part09/Part09Step11Controller.java
+++ src/org/etools/j1939_84/controllers/part09/Part09Step11Controller.java
@@ -3,19 +3,11 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
-import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
-
-import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
-import org.etools.j1939_84.bus.j1939.Lookup;
-import org.etools.j1939_84.bus.j1939.packets.DM20MonitorPerformanceRatioPacket;
-import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
-import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -63,70 +55,10 @@ public class Part09Step11Controller extends StepController {
     protected void run() throws Throwable {
         // 6.9.11.1.a. DS DM20 [(send Request (PGN 59904) for PGN 49664 (SPNs 3048-3049, 3066-3068)]) to ECUs that
         // responded earlier in this part with DM20 data.
-        List<Integer> addresses = getDataRepository().getObdModules()
-                                                     .stream()
-                                                     .map(OBDModuleInformation::getSourceAddress)
-                                                     .filter(a -> getDM20(a) != null)
-                                                     .collect(Collectors.toList());
-
-        var dsResults = addresses.stream()
-                                 .map(a -> getDiagnosticMessageModule().requestDM20(getListener(), a))
-                                 .collect(Collectors.toList());
-
-        var packets = filterPackets(dsResults);
-        var acks = filterAcks(dsResults);
-
-        // 6.9.11.2.a. Fail if ignition cycle is not equal to the value that it was earlier in Step 6.9.4.1.b
-        // (before DM11).
-        packets.stream()
-               .filter(p -> getDM20(p.getSourceAddress()) != null)
-               .filter(p -> getDM20(p.getSourceAddress()).getIgnitionCycles() != p.getIgnitionCycles())
-               .map(ParsedPacket::getModuleName)
-               .forEach(moduleName -> {
-                   addFailure("6.9.11.2.a - " + moduleName
-                           + " reported value for ignition cycles is not equal to the value from Step 6.9.4.1.b");
-               });
-
-        // 6.9.11.2.a. Fail if numerator or denominator is not equal to the value that it was earlier in Step 6.9.4.1.b
-        // (before DM11).
-        packets.stream()
-               .filter(p -> getDM20(p.getSourceAddress()) != null)
-               .filter(p -> !getDM20(p.getSourceAddress()).getRatios().equals(p.getRatios()))
-               .map(ParsedPacket::getModuleName)
-               .forEach(moduleName -> {
-                   addFailure("6.9.11.2.a - " + moduleName
-                           + " reported values for performance ratios not equal to the values from Step 6.9.4.1.b");
-               });
-
+        // 6.9.11.2.a. Fail if any value (ignition cycle, numerator, or denominator) is not equal to the value that it
+        // was earlier in Step 6.9.4.1.b (before DM11).
         // 6.9.11.2.b. Fail if any ECU now NACKs DM20 requests after previously providing data in 6.9.4.1.
-        acks.stream()
-            .filter(a -> a.getResponse() == NACK)
-            .filter(a -> getDM20(a.getSourceAddress()) != null)
-            .map(ParsedPacket::getModuleName)
-            .forEach(moduleName -> {
-                addFailure("6.9.11.2.b - " + moduleName
-                        + " now NACK'd DM20 request after previously providing data in 6.9.4.1");
-            });
-
         // 6.9.11.2.c. Fail if any NACK not received from an OBD ECU that did not provide a DM20 message.
-        packets.stream().map(ParsedPacket::getSourceAddress).forEach(addresses::remove);
-
-        acks.stream()
-            .filter(a -> a.getResponse() == NACK)
-            .map(ParsedPacket::getSourceAddress)
-            .forEach(addresses::remove);
-
-        addresses.stream()
-                 .distinct()
-                 .sorted()
-                 .map(Lookup::getAddressName)
-                 .forEach(moduleName -> {
-                     addFailure("6.9.11.2.c - OBD module " + moduleName + " did not provide a NACK for the DS query");
-                 });
-    }
-
-    private DM20MonitorPerformanceRatioPacket getDM20(int address) {
-        return get(DM20MonitorPerformanceRatioPacket.class, address);
     }
 
 }
