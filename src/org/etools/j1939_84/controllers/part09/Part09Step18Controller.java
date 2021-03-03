d067ae6618fdeee6fe0a7b75603659cc97d8acd4
Matt Gumbel <matt@soliddesign.net>
2021-03-03 15:34:21 -0500

    Implement Part 9 Step 18

--- src/org/etools/j1939_84/controllers/part09/Part09Step18Controller.java
+++ src/org/etools/j1939_84/controllers/part09/Part09Step18Controller.java
@@ -5,7 +5,10 @@ package org.etools.j1939_84.controllers.part09;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCodePacket;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -55,9 +58,31 @@ public class Part09Step18Controller extends StepController {
     protected void run() throws Throwable {
         // 6.9.18.1.a. DS DM23 [(send Request (PGN 59904) for PGN 64949 (SPNs 1213-1215, 1706, and 3038)]) to each OBD
         // ECU.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(a -> getDiagnosticMessageModule().requestDM23(getListener(), a))
+                                           .collect(Collectors.toList());
+
+        var packets = filterPackets(dsResults);
+
         // 6.9.18.2.a. Fail if any ECU reports a previously active DTC.
+        packets.stream()
+               .filter(DiagnosticTroubleCodePacket::hasDTCs)
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> {
+                   addFailure("6.9.18.2.a - " + moduleName + " reported a previously active DTC");
+               });
+
         // 6.9.18.2.b. Fail if any ECU does not report MIL off. See Section A.8 for allowed values.
+        packets.stream()
+               .filter(p -> isNotOff(p.getMalfunctionIndicatorLampStatus()))
+               .map(ParsedPacket::getModuleName)
+               .forEach(moduleName -> {
+                   addFailure("6.9.18.2.b - " + moduleName + " did not report MIL 'off'");
+               });
+
         // 6.9.18.2.c. Fail if NACK not received from OBD ECUs that did not provide DM23 message
+        checkForNACKsDS(packets, filterAcks(dsResults), "6.9.18.2.c");
     }
 
 }
