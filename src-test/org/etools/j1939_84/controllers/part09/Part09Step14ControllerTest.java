b0878424de16b7159cddb6eb8fbdf5ae4a5ae0a8
Matt Gumbel <matt@soliddesign.net>
2021-03-03 12:11:31 -0500

    Implement Part 9 Step 14

--- src-test/org/etools/j1939_84/controllers/part09/Part09Step14ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part09/Part09Step14ControllerTest.java
@@ -3,16 +3,31 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM27AllPendingDTCsPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +134,92 @@ public class Part09Step14ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dm27_0 = DM27AllPendingDTCsPacket.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM27(any(), eq(0))).thenReturn(BusResult.of(dm27_0));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM27(any(), eq(1))).thenReturn(BusResult.of(nack));
+
+        var dtc = DiagnosticTroubleCode.create(123, 1, 1, 1);
+        var dm27_2 = DM27AllPendingDTCsPacket.create(2, ON, ON, ON, ON, dtc);
+
+        when(diagnosticMessageModule.requestDM27(any())).thenReturn(RequestResult.of(dm27_0, dm27_2));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM27(any());
+        verify(diagnosticMessageModule).requestDM27(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM27(any(), eq(1));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+        assertEquals(List.of(), listener.getOutcomes());
+    }
+
+    @Test
+    public void testFailureForDTC() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dtc = DiagnosticTroubleCode.create(123, 1, 1, 1);
+        var dm27_0 = DM27AllPendingDTCsPacket.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM27(any(), eq(0))).thenReturn(BusResult.of(dm27_0));
+
+        when(diagnosticMessageModule.requestDM27(any())).thenReturn(RequestResult.of(dm27_0));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM27(any());
+        verify(diagnosticMessageModule).requestDM27(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.14.2.a - Engine #1 (0) reported a pending DTC");
+    }
+
+    @Test
+    public void testFailureForDifference() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        var dtc = DiagnosticTroubleCode.create(123, 1, 1, 1);
+        var dm27_0 = DM27AllPendingDTCsPacket.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM27(any())).thenReturn(RequestResult.of(dm27_0));
+
+        var dm27_1 = DM27AllPendingDTCsPacket.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM27(any(), eq(0))).thenReturn(BusResult.of(dm27_1));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM27(any());
+        verify(diagnosticMessageModule).requestDM27(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.14.4.a - Difference compared to data received during global request from Engine #1 (0)");
+    }
+
+    @Test
+    public void testFailureForNoNACK() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        when(diagnosticMessageModule.requestDM27(any())).thenReturn(RequestResult.of());
+        when(diagnosticMessageModule.requestDM27(any(), eq(0))).thenReturn(BusResult.empty());
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM27(any());
+        verify(diagnosticMessageModule).requestDM27(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.14.4.b - OBD module Engine #1 (0) did not provide a response to Global query and did not provide a NACK for the DS query");
     }
 
 }
