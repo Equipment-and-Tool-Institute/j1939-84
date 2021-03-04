96b865b756bd705e501d8bf558a3f3a3c91dc8e7
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-03 22:24:01 -0500

    Fix #370 - add part 09 step 21 and associated unit tests.

--- src/org/etools/j1939_84/controllers/part09/Part09Step21Controller.java
+++ src/org/etools/j1939_84/controllers/part09/Part09Step21Controller.java
@@ -3,6 +3,8 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static java.lang.String.format;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -54,7 +56,24 @@ public class Part09Step21Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.9.21.1.a. Global DM5 [(send Request (PGN 59904) for PGN 65230 (SPNs 1218-1219)]).
-        // 6.9.21.2.a. Fail if any OBD ECU reports > 0 active DTCs or > 0 previously active DTCs.
+        getDiagnosticMessageModule().requestDM5(getListener())
+                                    .getPackets()
+                                    .stream()
+                                    .peek(this::save)
+                                    .filter(p -> getDataRepository().isObdModule(p.getSourceAddress()))
+                                    // 6.9.21.2.a. Fail if any OBD ECU reports > 0 active DTCs or > 0 previously active
+                                    // DTCs.
+                                    .forEach(p -> {
+                                        if (p.getPreviouslyActiveCodeCount() != (byte) 0xFF
+                                                && p.getPreviouslyActiveCodeCount() > 0) {
+                                            addFailure(format("6.9.21.2.a - OBD ECU %s reported > 0 previously active DTCs count",
+                                                              p.getModuleName()));
+                                        }
+                                        if (p.getActiveCodeCount() != (byte) 0xFF && p.getActiveCodeCount() > 0) {
+                                            addFailure(format("6.9.21.2.a - OBD ECU %s reported > 0 active DTCs count",
+                                                              p.getModuleName()));
+                                        }
+                                    });
     }
 
 }
