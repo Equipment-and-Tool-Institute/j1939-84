51b565ceebb75b1b62431c092e6500a88941df53
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-03-04 09:11:01 -0500

    Implement Part 11 Step 9 (#701)

--- src-test/org/etools/j1939_84/controllers/part11/Part11Step09ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part11/Part11Step09ControllerTest.java
@@ -3,16 +3,28 @@
  */
 package org.etools.j1939_84.controllers.part11;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM28PermanentEmissionDTCPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -119,11 +131,94 @@ public class Part11Step09ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc = DiagnosticTroubleCode.create(123, 12, 1, 1);
+        obdModuleInformation.set(DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dtc1 = DiagnosticTroubleCode.create(123, 12, 0, 2);
+        var dm28 = DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc1);
+        when(diagnosticMessageModule.requestDM28(any(), eq(0))).thenReturn(BusResult.of(dm28));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM28(any(), eq(1))).thenReturn(BusResult.of(nack));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM28(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM28(any(), eq(1));
+
         assertEquals("", listener.getMessages());
         assertEquals("", listener.getResults());
+    }
+
+    @Test
+    public void testFailureForNoDTCs() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        obdModuleInformation.set(DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dm28 = DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM28(any(), eq(0))).thenReturn(BusResult.of(dm28));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM28(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.11.9.2.a - NO ECU reported a permanent DTC");
+    }
+
+    @Test
+    public void testFailureForDifferentDTCs() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc = DiagnosticTroubleCode.create(123, 12, 1, 1);
+        obdModuleInformation.set(DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dtc1 = DiagnosticTroubleCode.create(456, 12, 0, 2);
+        var dm28 = DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc1);
+        when(diagnosticMessageModule.requestDM28(any(), eq(0))).thenReturn(BusResult.of(dm28));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM28(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.11.9.2.b - Engine #1 (0) reported a different DTC than as reported in DM28 in part 10");
+    }
+
+    @Test
+    public void testFailureForNoNACK() {
+        OBDModuleInformation obdModuleInformation = new OBDModuleInformation(0);
+        var dtc = DiagnosticTroubleCode.create(123, 12, 1, 1);
+        obdModuleInformation.set(DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc));
+        dataRepository.putObdModule(obdModuleInformation);
+
+        var dtc1 = DiagnosticTroubleCode.create(123, 12, 0, 2);
+        var dm28 = DM28PermanentEmissionDTCPacket.create(0, OFF, OFF, OFF, OFF, dtc1);
+        when(diagnosticMessageModule.requestDM28(any(), eq(0))).thenReturn(BusResult.of(dm28));
+
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+        when(diagnosticMessageModule.requestDM28(any(), eq(1))).thenReturn(BusResult.empty());
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM28(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM28(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.11.9.2.c - OBD module Engine #2 (1) did not provide a NACK for the DS query");
     }
 
 }
