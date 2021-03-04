e5df0b9d34391d12aa78360d1f74a13a26d85f9a
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-03-04 09:11:13 -0500

    Implement Part 11 Step 10 (#702)

--- src/org/etools/j1939_84/controllers/part11/Part11Step10Controller.java
+++ src/org/etools/j1939_84/controllers/part11/Part11Step10Controller.java
@@ -3,9 +3,13 @@
  */
 package org.etools.j1939_84.controllers.part11;
 
+import static org.etools.j1939_84.modules.DiagnosticMessageModule.getCompositeSystems;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.MonitoredSystem;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,8 +58,24 @@ public class Part11Step10Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.11.10.1.a. DS DM5 [(send Request (PGN 59904) for PGN 65230 (SPNs 1218-1223)]) to each OBD ECU.
+        var packets = filterPackets(getDataRepository().getObdModuleAddresses()
+                                                       .stream()
+                                                       .map(a -> getDiagnosticMessageModule().requestDM5(getListener(),
+                                                                                                         a))
+                                                       .collect(Collectors.toList()));
+
         // 6.11.10.1.b. Record all data (i.e., which monitors are supported and complete or supported and incomplete).
+        packets.forEach(this::save);
+
         // 6.11.10.1.c. Display monitor readiness composite value in log
+        if (!packets.isEmpty()) {
+            getListener().onResult("");
+            getListener().onResult("Vehicle Composite of DM5:");
+            getCompositeSystems(packets, true).stream()
+                                              .map(MonitoredSystem::toString)
+                                              .forEach(string -> getListener().onResult(string));
+        }
+
     }
 
 }
