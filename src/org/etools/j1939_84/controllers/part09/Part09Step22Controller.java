a57f13c72904a5bc252ee9108ba64db5e0d148df
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-04 13:08:28 -0500

    Fix #371 - add part 09 step 22 and associated unit tests.

--- src/org/etools/j1939_84/controllers/part09/Part09Step22Controller.java
+++ src/org/etools/j1939_84/controllers/part09/Part09Step22Controller.java
@@ -3,8 +3,11 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static java.lang.String.format;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
@@ -54,12 +57,40 @@ public class Part09Step22Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.9.22.1.a. Global DM2 [(send Request (PGN 59904) for PGN 65227 (SPNs 1213-1215, 1706, and 3038)]).
-        // 6.9.22.2.a. (if supported) Fail if any ECU does not report MIL off or MIL not supported. See Section A.8 for
-        // allowed values.
-        // 6.9.22.2.b. (if supported) Fail if any OBD ECU reports a previously active DTC.
-        // 6.9.22.3.a. DS DM2 to each OBD ECU.
-        // 6.9.22.4.a. (if supported) Fail if any difference compared to data received during global request.
-        // 6.9.22.4.b. (if supported) Fail if NACK not received from OBD ECUs that did not respond to global query.
-    }
+        var globalPackets = getDiagnosticMessageModule().requestDM2(getListener())
+                                                        .getPackets()
+                                                        .stream()
+                                                        .peek(this::save)
+                                                        .filter(p -> getDataRepository().isObdModule(p.getSourceAddress()))
+                                                        .peek(p -> {
+                                                            // 6.9.22.2.a. (if supported) Fail if any ECU does not
+                                                            // report
+                                                            // MIL off or MIL not supported.
+                                                            // See Section A.8 for allowed values.
+                                                            if (isNotOff(p.getMalfunctionIndicatorLampStatus())) {
+                                                                addFailure(format("6.9.22.2.a - ECU %s reported MIL off",
+                                                                                  p.getModuleName()));
+                                                            }
+                                                            // 6.9.22.2.b. (if supported) Fail if any OBD ECU reports a
+                                                            // previously active DTC.
+                                                            // DM2 DTC are only previously active DTCs
+                                                            if (p.hasDTCs()) {
+                                                                addFailure(format("6.9.22.2.b - ECU %s reported a previously active DTC",
+                                                                                  p.getModuleName()));
 
+                                                            }
+                                                        })
+                                                        .collect(Collectors.toList());
+        // 6.9.22.3.a. DS DM2 to each OBD ECU.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(address -> getDiagnosticMessageModule().requestDM2(getListener(),
+                                                                                                   address))
+                                           .collect(Collectors.toList());
+
+        // 6.9.22.4.a. (if supported) Fail if any difference compared to data received during global request.
+        compareRequestPackets(globalPackets, filterPackets(dsResults), "6.9.22.4.a");
+        // 6.9.22.4.b. (if supported) Fail if NACK not received from OBD ECUs that did not respond to global query.
+        checkForNACKsGlobal(globalPackets, filterAcks(dsResults), "6.9.22.4.b");
+    }
 }
