cec029f0b3383b9b42ba5e0b9267f334b414bbb7
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-03-05 14:23:48 -0500

    Implement Part 12 Step 2 (#710)

--- src/org/etools/j1939_84/controllers/part12/Part12Step02Controller.java
+++ src/org/etools/j1939_84/controllers/part12/Part12Step02Controller.java
@@ -3,9 +3,15 @@
  */
 package org.etools.j1939_84.controllers.part12;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.CompositeSystem;
+import org.etools.j1939_84.bus.j1939.packets.DM26TripDiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM5DiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.MonitoredSystem;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,9 +60,65 @@ public class Part12Step02Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.12.2.1.a. DS DM26 [(send Request (PGN 59904) for PGN 64952 (SPNs 3303-3305)]) to each OBD ECU.
+        var dsResults = getDataRepository().getObdModuleAddresses()
+                                           .stream()
+                                           .map(a -> getDiagnosticMessageModule().requestDM26(getListener(), a))
+                                           .collect(Collectors.toList());
+
+        var packets = filterRequestResultPackets(dsResults);
+
         // 6.12.2.2.a. Fail if any supported monitor (except CCM) that was “0 = complete this cycle” in part 11 is not
         // reporting “1 = not complete this cycle.”.
+        packets.forEach(this::reportNotCompleteSystems);
+
         // 6.12.2.2.b. Fail if NACK not received from OBD ECUs that did not provide a DM26 message
+        checkForNACKsDS(packets, filterRequestResultAcks(dsResults), "6.12.2.2.b");
+
+        // Save packets for next step
+        packets.forEach(this::save);
+    }
+
+    private void reportNotCompleteSystems(DM26TripDiagnosticReadinessPacket currentDM26) {
+        int address = currentDM26.getSourceAddress();
+        String moduleName = currentDM26.getModuleName();
+
+        var supportedSystems = getSupportedSystems(address);
+
+        var previouslyCompleteSupportedSystems = get(DM26TripDiagnosticReadinessPacket.class,
+                                                     address)
+                                                             .getMonitoredSystems()
+                                                             .stream()
+                                                             .filter(s -> s.getStatus().isComplete())
+                                                             .map(MonitoredSystem::getId)
+                                                             .filter(supportedSystems::contains)
+                                                             .collect(Collectors.toList());
+
+        currentDM26.getMonitoredSystems()
+                   .stream()
+                   .filter(s -> !s.getStatus().isComplete())
+                   .map(MonitoredSystem::getId)
+                   .filter(previouslyCompleteSupportedSystems::contains)
+                   .map(CompositeSystem::getName)
+                   .map(String::trim)
+                   .sorted()
+                   .forEach(system -> {
+                       addFailure("6.12.2.2.a - " + moduleName + " reported " + system
+                               + " as 'not complete this cycle' when it reported it as 'complete this cycle' in part 11");
+                   });
+    }
+
+    private List<CompositeSystem> getSupportedSystems(int address) {
+        var dm5 = get(DM5DiagnosticReadinessPacket.class, address);
+        if (dm5 == null) {
+            return List.of();
+        }
+
+        return dm5.getMonitoredSystems()
+                  .stream()
+                  .filter(s -> s.getStatus().isEnabled())
+                  .map(MonitoredSystem::getId)
+                  .filter(id -> id != CompositeSystem.COMPREHENSIVE_COMPONENT)
+                  .collect(Collectors.toList());
     }
 
 }
