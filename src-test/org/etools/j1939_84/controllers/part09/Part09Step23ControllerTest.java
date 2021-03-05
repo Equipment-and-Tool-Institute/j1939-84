1af92c6c1919e91baefde04bccc5704ca2fad2ae
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-05 08:39:45 -0500

    Add Part 09 Step 23 (#707)
    * Fix #372 - add part 09 step 23 and associated unit tests.
    
    * Fix #372 - add part 09 step 23 and associated unit tests.
    
    * Fix #372 - add part 09 step 23 and associated unit tests.

--- src-test/org/etools/j1939_84/controllers/part09/Part09Step23ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part09/Part09Step23ControllerTest.java
@@ -3,16 +3,28 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ALTERNATE_OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.NOT_SUPPORTED;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.DM1ActiveDTCsPacket;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -118,12 +130,109 @@ public class Part09Step23ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testHappyPathNoFailures() {
+    public void testNoFailures() {
+        var dm1_0 = DM1ActiveDTCsPacket.create(0, OFF, OFF, OFF, OFF);
+        var dm1_21 = DM1ActiveDTCsPacket.create(0x21, OFF, OFF, OFF, OFF);
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        when(diagnosticMessageModule.readDM1(any())).thenReturn(List.of(dm1_0, dm1_21));
 
         runTest();
 
+        verify(diagnosticMessageModule).readDM1(any());
+
         assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
         assertEquals("", listener.getResults());
     }
 
+    @Test
+    public void testPacketsEmpty() {
+        var dm1_0 = DM1ActiveDTCsPacket.create(0, OFF, OFF, OFF, OFF);
+        var dm1_21 = DM1ActiveDTCsPacket.create(0x21, OFF, OFF, OFF, OFF);
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        when(diagnosticMessageModule.readDM1(any())).thenReturn(List.of(dm1_0, dm1_21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).readDM1(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+    }
+
+    @Test
+    public void testNoFailuresAlternateValues() {
+        var dm1_0 = DM1ActiveDTCsPacket.create(0, ALTERNATE_OFF, OFF, OFF, OFF);
+        var dm1_21 = DM1ActiveDTCsPacket.create(0x21, NOT_SUPPORTED, OFF, OFF, OFF);
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        when(diagnosticMessageModule.readDM1(any())).thenReturn(List.of(dm1_0, dm1_21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).readDM1(any());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.a - ECU Engine #1 (0) reported MIL status of alternate off");
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+    }
+
+    @Test
+    public void testFailureForNoResponse() {
+        when(diagnosticMessageModule.readDM1(any())).thenReturn(List.of());
+
+        runTest();
+
+        verify(diagnosticMessageModule).readDM1(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.9.23.2.c - No OBD ECU provided a DM1");
+    }
+
+    @Test
+    public void testFailures() {
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+        var dm1_0 = DM1ActiveDTCsPacket.create(0, ON, OFF, OFF, OFF, dtc);
+        var dm1_21 = DM1ActiveDTCsPacket.create(0x21, ON, OFF, OFF, OFF);
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        when(diagnosticMessageModule.readDM1(any())).thenReturn(List.of(dm1_0, dm1_21));
+
+        runTest();
+
+        verify(diagnosticMessageModule).readDM1(any());
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.a - ECU Engine #1 (0) reported MIL status of on");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.b - ECU Engine #1 (0) reported a previously active DTC");
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.a - ECU Body Controller (33) reported MIL status of on");
+
+    }
 }
--- src/org/etools/j1939_84/controllers/part09/Part09Step23Controller.java
+++ src/org/etools/j1939_84/controllers/part09/Part09Step23Controller.java
@@ -3,9 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static java.lang.String.format;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.NOT_SUPPORTED;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM1ActiveDTCsPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -54,9 +61,33 @@ public class Part09Step23Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.9.23.1.a. Receive DM1 broadcast [(PGN 65226 (SPNs 1213-1215, 1706, and 3038)]).
-        // 6.9.23.2.a. Fail if any ECU does not report MIL off or MIL not supported.
-        // 6.9.23.2.b. Fail if any ECU reports an active DTC.
+        List<DM1ActiveDTCsPacket> packets = getDiagnosticMessageModule().readDM1(getListener())
+                                                                        .stream()
+                                                                        .peek(this::save)
+                                                                        .peek(p -> {
+                                                                            // 6.9.23.2.a. Fail if any ECU does not
+                                                                            // report MIL off or MIL not supported.
+                                                                            if (p.getMalfunctionIndicatorLampStatus() != OFF
+                                                                                    &&
+                                                                                    p.getMalfunctionIndicatorLampStatus() != NOT_SUPPORTED) {
+                                                                                addFailure(format("6.9.22.2.a - ECU %s reported MIL status of %s",
+                                                                                                  p.getModuleName(),
+                                                                                                  p.getMalfunctionIndicatorLampStatus()));
+                                                                            }
+                                                                            // 6.9.23.2.b. Fail if any ECU reports an
+                                                                            // active DTC.
+                                                                            if (p.hasDTCs()) {
+                                                                                addFailure(format("6.9.22.2.b - ECU %s reported a previously active DTC",
+                                                                                                  p.getModuleName()));
+                                                                            }
+                                                                        })
+                                                                        .filter(p -> getDataRepository().isObdModule(p.getSourceAddress()))
+                                                                        .collect(Collectors.toList());
+
         // 6.9.23.2.c. Fail if no OBD ECU provides DM1.
+        if (packets.isEmpty()) {
+            addFailure("6.9.23.2.c - No OBD ECU provided a DM1");
+        }
     }
 
 }
