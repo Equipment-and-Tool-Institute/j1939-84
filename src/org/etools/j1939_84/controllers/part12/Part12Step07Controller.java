ad220de5d33b3de6393339ed92fa5d367ec5368c
Matt Gumbel <matt@soliddesign.net>
2021-03-05 12:51:40 -0500

    Implement Part 12 Step 7

--- src/org/etools/j1939_84/controllers/part12/Part12Step07Controller.java
+++ src/org/etools/j1939_84/controllers/part12/Part12Step07Controller.java
@@ -6,6 +6,8 @@ package org.etools.j1939_84.controllers.part12;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import org.etools.j1939_84.bus.j1939.packets.DM21DiagnosticReadinessPacket;
+import org.etools.j1939_84.bus.j1939.packets.ParsedPacket;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.modules.BannerModule;
@@ -53,11 +55,50 @@ public class Part12Step07Controller extends StepController {
 
     @Override
     protected void run() throws Throwable {
-        // 6.12.7.1.a. Global DM21 [(send Request (PGN 59904) for PGN 49408 (SPNs 3294, 3296)]).
+        // 6.12.7.1.a. Global DM21 (send Request (PGN 59904) for PGN 49408 (SPNs 3294, 3296)].
+        var dm21s = getDiagnosticMessageModule().requestDM21(getListener()).getPackets();
+
         // 6.12.7.2.a. Fail if any ECU reports distance SCC (SPN 3294) > 0.
+        dm21s.stream()
+             .filter(p -> p.getKmSinceDTCsCleared() > 0)
+             .map(ParsedPacket::getModuleName)
+             .forEach(moduleName -> {
+                 addFailure("6.12.7.2.a - " + moduleName + " reported distance SCC > 0");
+             });
+
         // 6.12.7.2.b. Fail if any ECU reports < 10 minutes for time SCC (SPN 3296), if supported.
+        dm21s.stream()
+             .filter(p -> p.getMinutesSinceDTCsCleared() != ParsedPacket.NOT_AVAILABLE)
+             .filter(p -> p.getMinutesSinceDTCsCleared() < 10)
+             .map(ParsedPacket::getModuleName)
+             .forEach(moduleName -> {
+                 addFailure("6.12.7.2.b - " + moduleName + " reported < 10 minutes for time SCC");
+             });
+
         // 6.12.7.2.c. If more than one ECU responds, fail if values reported for time SCC differ by > 1 minute.
+        var max = dm21s.stream()
+                       .map(DM21DiagnosticReadinessPacket::getMinutesSinceDTCsCleared)
+                       .filter(t -> t != ParsedPacket.NOT_AVAILABLE)
+                       .mapToInt(Double::intValue)
+                       .max()
+                       .orElse(0);
+
+        var min = dm21s.stream()
+                       .map(DM21DiagnosticReadinessPacket::getMinutesSinceDTCsCleared)
+                       .filter(t -> t != ParsedPacket.NOT_AVAILABLE)
+                       .mapToInt(Double::intValue)
+                       .min()
+                       .orElse(0);
+
+        if ((max - min) > 1) {
+            addFailure("6.12.7.2.c - More than one ECU responded and values reported for time SCC differ by > 1 minute");
+        }
+
         // 6.12.7.2.d. Fail if no OBD ECU provides a DM21 message.
+        var noObdResponse = dm21s.stream().noneMatch(p -> isObdModule(p.getSourceAddress()));
+        if (noObdResponse) {
+            addFailure("6.12.7.2.d - No OBD ECU provided a DM21 message");
+        }
     }
 
 }
