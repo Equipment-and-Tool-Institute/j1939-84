057bf1d668d12d3fc57d304a6c514903309ba9c5
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-05 07:25:51 -0500

    Add Part 09 Step 22 (#705)
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.
    
    * Fix #371 - add part 09 step 22 and associated unit tests.

--- src-test/org/etools/j1939_84/controllers/part09/Part09Step22ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part09/Part09Step22ControllerTest.java
@@ -3,16 +3,31 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
+import static org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket.Response.NACK;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.OFF;
+import static org.etools.j1939_84.bus.j1939.packets.LampStatus.ON;
+import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.etools.j1939_84.bus.j1939.BusResult;
 import org.etools.j1939_84.bus.j1939.J1939;
+import org.etools.j1939_84.bus.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939_84.bus.j1939.packets.DM2PreviouslyActiveDTC;
+import org.etools.j1939_84.bus.j1939.packets.DiagnosticTroubleCode;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
+import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.RequestResult;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -118,12 +133,161 @@ public class Part09Step22ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testHappyPathNoFailures() {
+    public void testEmptyGlobalPacketsFailure() {
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, List.of(), List.of()));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(BusResult.of(dm2));
+
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(BusResult.of(nack));
 
         runTest();
 
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.4.b - OBD module Engine #1 (0) did not provide a response to Global query and did not provide a NACK for the DS query");
+
         assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
         assertEquals("", listener.getResults());
+    }
+
+    @Test
+    public void testNoFailures() {
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        var nack = AcknowledgmentPacket.create(1, NACK);
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(new BusResult<>(false, nack));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+    }
+
+    @Test
+    public void testFailureForNoNack() {
+
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+        dataRepository.putObdModule(new OBDModuleInformation(1));
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        when(diagnosticMessageModule.requestDM2(any(), eq(1))).thenReturn(new BusResult<>(true));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+        verify(diagnosticMessageModule).requestDM2(any(), eq(1));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.4.b - OBD module Engine #2 (1) did not provide a response to Global query and did not provide a NACK for the DS query");
+    }
+
+    @Test
+    public void testFailureForDTC() {
+        OBDModuleInformation obdModule = new OBDModuleInformation(0);
+        dataRepository.putObdModule(obdModule);
+
+        var dtc = DiagnosticTroubleCode.create(123, 12, 0, 1);
+        var dm2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF, dtc);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.b - ECU Engine #1 (0) reported a previously active DTC");
+    }
+
+    @Test
+    public void testFailureForMILNotOff() {
+        OBDModuleInformation obdModule = new OBDModuleInformation(0);
+        dataRepository.putObdModule(obdModule);
+
+        var dm2 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, dm2));
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dm2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.2.a - ECU Engine #1 (0) did not report MIL off");
+    }
+
+    @Test
+    public void testFailureGlobalAndDSDifference() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var globalDM2 = DM2PreviouslyActiveDTC.create(0, OFF, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any())).thenReturn(new RequestResult<>(false, globalDM2));
+
+        var dsDM2 = DM2PreviouslyActiveDTC.create(0, ON, OFF, OFF, OFF);
+        when(diagnosticMessageModule.requestDM2(any(), eq(0))).thenReturn(new BusResult<>(false, dsDM2));
+
+        runTest();
+
+        verify(diagnosticMessageModule).requestDM2(any());
+        verify(diagnosticMessageModule).requestDM2(any(), eq(0));
+
+        assertEquals("", listener.getMessages());
+        assertEquals("", listener.getMilestones());
+        assertEquals("", listener.getResults());
+
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        FAIL,
+                                        "6.9.22.4.a - Difference compared to data received during global request from Engine #1 (0)");
     }
 
 }
