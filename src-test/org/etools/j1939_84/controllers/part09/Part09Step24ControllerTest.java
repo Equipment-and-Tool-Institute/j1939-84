d03576d74add3f9b7f5c317e5d124939bb796315
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-05 14:10:27 -0500

    Revert "Fix #373 - add part 09 step 24 and associated unit tests."
    This reverts commit 801f5332c9f6f9ca8fce6f340c7499a607cc4104.

--- src-test/org/etools/j1939_84/controllers/part09/Part09Step24ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part09/Part09Step24ControllerTest.java
@@ -3,28 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part09;
 
-import static org.etools.j1939_84.bus.j1939.packets.DM33EmissionIncreasingAECDActiveTime.create;
-import static org.etools.j1939_84.model.Outcome.FAIL;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.concurrent.Executor;
 
 import org.etools.j1939_84.bus.j1939.J1939;
-import org.etools.j1939_84.bus.j1939.packets.EngineHoursTimer;
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.ResultsListener;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
-import org.etools.j1939_84.model.FuelType;
-import org.etools.j1939_84.model.OBDModuleInformation;
-import org.etools.j1939_84.model.RequestResult;
-import org.etools.j1939_84.model.VehicleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.DateTimeModule;
 import org.etools.j1939_84.modules.DiagnosticMessageModule;
@@ -131,183 +119,11 @@ public class Part09Step24ControllerTest extends AbstractControllerTest {
 
     @Test
     public void testHappyPathNoFailures() {
-        var dsPacket = create(0, EngineHoursTimer.create(1, 4, 10));
-
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        obdModule.set(dsPacket);
-        dataRepository.putObdModule(obdModule);
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.of(dsPacket));
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.DSL);
-        dataRepository.setVehicleInformation(vehInfo);
 
         runTest();
 
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
         assertEquals("", listener.getMessages());
-        assertEquals(List.of(), listener.getOutcomes());
+        assertEquals("", listener.getResults());
     }
 
-    @Test
-    public void testNoResponsesCIEngine() {
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.empty());
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.GAS);
-        dataRepository.setVehicleInformation(vehInfo);
-
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        var previousPacket = create(0, EngineHoursTimer.create(1, 4, 10));
-        obdModule.set(previousPacket);
-        dataRepository.putObdModule(obdModule);
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
-        assertEquals("", listener.getMessages());
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.c. - OBD module Engine #1 (0) did not provide a NACK for the DS query");
-
-    }
-
-    @Test
-    public void testdifferntNumberOfTimers() {
-        var timer1 = EngineHoursTimer.create(1, 4, 10);
-        var timer2 = EngineHoursTimer.create(1, 7, 9);
-        var previousPacket = create(0, timer1);
-        var packet = create(0, timer1, timer2);
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.of(packet));
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.DSL);
-        dataRepository.setVehicleInformation(vehInfo);
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        obdModule.set(previousPacket);
-        dataRepository.putObdModule(obdModule);
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
-        assertEquals("", listener.getMessages());
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.a - ECU Engine #1 (0) reported a different number EI-AECD here (2) and reported (1) in part 2");
-    }
-
-    @Test
-    public void testWarnValueOfFB() {
-        var timer1 = EngineHoursTimer.create(1, 4, 10);
-        var timer2 = EngineHoursTimer.create(1, 7, 9);
-        var packet = create(0, timer1);
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.of(packet));
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.DSL);
-        dataRepository.setVehicleInformation(vehInfo);
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        obdModule.set(create(0, timer2));
-        dataRepository.putObdModule(obdModule);
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
-        assertEquals("", listener.getMessages());
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported differing timers than those reported in 6.9.7.1");
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported 6.9.7.1 timer 1 value less than previously observed in 6.9.7.1");
-    }
-
-    @Test
-    public void testFailTimer2Values() {
-        var globalPacket = create(0, EngineHoursTimer.create(1, 4, 10));
-        var dsPacket = create(0, EngineHoursTimer.create(1, 7, 9));
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.of(dsPacket));
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.DSL);
-        dataRepository.setVehicleInformation(vehInfo);
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        obdModule.set(globalPacket);
-        dataRepository.putObdModule(obdModule);
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
-        assertEquals("", listener.getMessages());
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported 6.9.7.1 timer 2 value less than previously observed in 6.9.7.1");
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported differing timers than those reported in 6.9.7.1");
-    }
-
-    @Test
-    public void testFailTimer1Values() {
-        var globalPacket = create(0, EngineHoursTimer.create(1, 7, 9));
-        var dsPacket = create(0, EngineHoursTimer.create(1, 3, 10));
-
-        when(diagnosticMessageModule.requestDM33(any(), eq(0))).thenReturn(RequestResult.of(dsPacket));
-
-        var vehInfo = new VehicleInformation();
-        vehInfo.setEngineModelYear(2020);
-        vehInfo.setFuelType(FuelType.DSL);
-        dataRepository.setVehicleInformation(vehInfo);
-        OBDModuleInformation obdModule = new OBDModuleInformation(0);
-        obdModule.set(globalPacket);
-        dataRepository.putObdModule(obdModule);
-
-        runTest();
-
-        verify(diagnosticMessageModule).requestDM33(any(), eq(0x00));
-
-        assertEquals("", listener.getResults());
-        assertEquals("", listener.getMessages());
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported 6.9.7.1 timer 1 value less than previously observed in 6.9.7.1");
-
-        verify(mockListener).addOutcome(PART_NUMBER,
-                                        STEP_NUMBER,
-                                        FAIL,
-                                        "6.9.24.2.b - ECU Engine #1 (0) reported differing timers than those reported in 6.9.7.1");
-    }
 }
