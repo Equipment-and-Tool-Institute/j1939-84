ecde7a019fbe653c355ca4f412eb6b183297d727
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-03-07 09:40:39 -0500

    Fix #625 - Add part 08 step 16 and associated unit tests. (#733)

--- src/org/etools/j1939_84/controllers/part08/Part08Step16Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step16Controller.java
@@ -3,6 +3,12 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import static java.lang.String.format;
+import static org.etools.j1939_84.J1939_84.NL;
+import static org.etools.j1939_84.controllers.ResultsListener.MessageType.WARNING;
+import static org.etools.j1939_84.model.KeyState.KEY_OFF;
+import static org.etools.j1939_84.model.KeyState.KEY_ON_ENGINE_OFF;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -54,12 +60,40 @@ public class Part08Step16Controller extends StepController {
     @Override
     protected void run() throws Throwable {
         // 6.8.16.1.a Turn the engine off.
+        ensureKeyStateIs(KEY_OFF);
+
         // 6.8.16.1.b Wait manufacturer's recommended interval.
+        waitForManufacturerInterval("6.8.16.1.b", KEY_OFF);
+
         // 6.8.16.1.c With the key in the off position remove the implanted Fault B, according to the manufacturer’s
         // instructions for restoring the system to a fault- free operating condition.
+        waitForFault("6.8.16.1.c");
+
         // 6.8.16.1.d Turn the ignition key to the ON position.
+        ensureKeyStateIs(KEY_ON_ENGINE_OFF);
+
         // 6.8.16.1.e Do not start engine.
         // 6.8.16.1.f Proceed with part 9.
+        displayDoNotStartEngine("6.8.16.1.e & f");
+    }
+
+    private void waitForFault(String stepId) throws InterruptedException {
+        incrementProgress(format("%s - With the key in the off position remove the implanted Fault B, according to the manufacturer’s"
+                + NL + "instructions for restoring the system to a fault- free operating condition.",
+                                 stepId));
+        String message = "With the key in the off position remove the implanted Fault B, according to the manufacturer’s"
+                + NL + "instructions for restoring the system to a fault- free operating condition." + NL;
+        message += "Press OK to continue the testing.";
+        displayInstructionAndWait(message, stepId, WARNING);
+    }
+
+    private void displayDoNotStartEngine(String stepId) throws InterruptedException {
+        incrementProgress(format("%s - Do Not Start Engine - proceeding with part 9",
+                                 stepId));
+        String message = "Do Not Start Engine." + NL;
+        message += "Proceeding with part 9." + NL;
+        message += "Press OK to continue the testing.";
+        displayInstructionAndWait(message, stepId, WARNING);
     }
 
 }
