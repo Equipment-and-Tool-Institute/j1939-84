53a10a39f148bf1ff704eec2b8a94e8a373ff34a
Matt Gumbel <matt@soliddesign.net>
2021-03-18 20:00:50 -0400

    Fix verbiage in Part 11 Step 1 Controller

--- src-test/org/etools/j1939_84/controllers/part11/Part11Step01ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part11/Part11Step01ControllerTest.java
@@ -128,7 +128,7 @@ public class Part11Step01ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testEngineAlreadyRunning() {
+    public void testEngineAtKeyState() {
         when(engineSpeedModule.getKeyState()).thenReturn(KEY_ON_ENGINE_RUNNING);
         when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("0.0 RPMs");
 
@@ -137,8 +137,7 @@ public class Part11Step01ControllerTest extends AbstractControllerTest {
         verify(engineSpeedModule, atLeastOnce()).getEngineSpeedAsString();
         verify(engineSpeedModule, atLeastOnce()).getKeyState();
 
-        String expectedMessages = "";
-        assertEquals(expectedMessages, listener.getMessages());
+        assertEquals("Step 6.11.1.2.a - Waiting for engine start", listener.getMessages());
 
         String expectedResults = "";
         expectedResults += "Initial Engine Speed = 0.0 RPMs" + NL;
@@ -147,40 +146,43 @@ public class Part11Step01ControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testKeyOffTransitionsToKeyOnEngineRunning() {
-        when(engineSpeedModule.getKeyState()).thenReturn(KEY_ON_ENGINE_OFF, KEY_ON_ENGINE_OFF, KEY_ON_ENGINE_RUNNING);
-        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("0.0 RPMs", "500.0 RPMs");
+    public void testIntoKeyState() {
+        when(engineSpeedModule.getKeyState()).thenReturn(KEY_ON_ENGINE_OFF,
+                                                         KEY_ON_ENGINE_OFF,
+                                                         KEY_ON_ENGINE_RUNNING);
+        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("500.0 RPMs", "0.0 RPMs");
 
         runTest();
 
         verify(engineSpeedModule, atLeastOnce()).getEngineSpeedAsString();
         verify(engineSpeedModule, atLeastOnce()).getKeyState();
 
-        verify(mockListener).onUrgentMessage(eq("Please turn Key ON/Engine RUNNING"),
-                                             eq("Adjust Key Switch"),
+        verify(mockListener).onUrgentMessage(eq("Please start the engine"),
+                                             eq("Step 6.11.1.2.a"),
                                              eq(WARNING),
                                              any());
 
-        String expectedMessages = "Waiting for Key ON/Engine RUNNING...";
+        String expectedMessages = "Step 6.11.1.2.a - Waiting for engine start" + NL;
+        expectedMessages += "Step 6.11.1.2.a - Waiting for engine start...";
         assertEquals(expectedMessages, listener.getMessages());
 
         String expectedResults = "";
-        expectedResults += "Initial Engine Speed = 0.0 RPMs" + NL;
-        expectedResults += "Final Engine Speed = 500.0 RPMs" + NL;
+        expectedResults += "Initial Engine Speed = 500.0 RPMs" + NL;
+        expectedResults += "Final Engine Speed = 0.0 RPMs" + NL;
         assertEquals(expectedResults, listener.getResults());
     }
 
     @Test
     public void testUserQuits() {
-        when(engineSpeedModule.getKeyState()).thenReturn(KEY_ON_ENGINE_OFF, KEY_ON_ENGINE_OFF, KEY_ON_ENGINE_RUNNING);
-        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("0.0 RPMs", "500.0 RPMs");
+        when(engineSpeedModule.getKeyState()).thenReturn(KEY_ON_ENGINE_OFF);
+        when(engineSpeedModule.getEngineSpeedAsString()).thenReturn("500.0 RPMs");
 
         doAnswer((Answer<Void>) invocation -> {
             ((QuestionListener) invocation.getArguments()[3]).answered(CANCEL);
             return null;
         }).when(mockListener)
-          .onUrgentMessage(eq("Please turn Key ON/Engine RUNNING"),
-                           eq("Adjust Key Switch"),
+          .onUrgentMessage(eq("Please start the engine"),
+                           eq("Step 6.11.1.2.a"),
                            eq(WARNING),
                            any());
 
@@ -189,17 +191,25 @@ public class Part11Step01ControllerTest extends AbstractControllerTest {
         verify(engineSpeedModule, atLeastOnce()).getEngineSpeedAsString();
         verify(engineSpeedModule, atLeastOnce()).getKeyState();
 
-        verify(mockListener).onUrgentMessage(eq("Please turn Key ON/Engine RUNNING"),
-                                             eq("Adjust Key Switch"),
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        ABORT,
+                                        "User cancelled testing at Part " + PART_NUMBER + " Step " + STEP_NUMBER);
+        verify(mockListener).onUrgentMessage(eq("Please start the engine"),
+                                             eq("Step 6.11.1.2.a"),
                                              eq(WARNING),
                                              any());
 
-        String userCancelledMessage = "User cancelled testing at Part " + PART_NUMBER + " Step " + STEP_NUMBER;
+        String userCancelledMessage = "Step 6.11.1.2.a - Waiting for engine start" + NL;
+        userCancelledMessage += "User cancelled testing at Part " + PART_NUMBER + " Step " + STEP_NUMBER;
         assertEquals(userCancelledMessage, listener.getMessages());
 
-        assertEquals("Initial Engine Speed = 0.0 RPMs" + NL, listener.getResults());
+        assertEquals("Initial Engine Speed = 500.0 RPMs" + NL, listener.getResults());
 
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, ABORT, userCancelledMessage);
+        verify(mockListener).addOutcome(PART_NUMBER,
+                                        STEP_NUMBER,
+                                        ABORT,
+                                        "User cancelled testing at Part " + PART_NUMBER + " Step " + STEP_NUMBER);
     }
 
 }
