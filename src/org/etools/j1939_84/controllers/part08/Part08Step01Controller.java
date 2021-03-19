a5f7b7672f2f806d8507c59cfb6b0d7fed2763c2
Matt Gumbel <gumbelmj@users.noreply.github.com>
2021-03-19 07:03:08 -0400

    Fix verbiage in Part 8 Step 1 Controller (#792)

--- src/org/etools/j1939_84/controllers/part08/Part08Step01Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step01Controller.java
@@ -58,7 +58,7 @@ public class Part08Step01Controller extends StepController {
         // 6.8.1.1.a Gather broadcast data for engine speed (e.g., SPN 190)
         // 6.8.1.2.a If after 5 seconds engine speed is < 400 rpm, prompt/warn operator to confirm engine is running and
         // then press enter.
-        ensureKeyStateIs(KEY_ON_ENGINE_RUNNING);
+        ensureKeyStateIs(KEY_ON_ENGINE_RUNNING, "6.8.1.2.a");
     }
 
 }
