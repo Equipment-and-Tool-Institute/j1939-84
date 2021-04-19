47cb099b5b5924c470ab4f418d0e3c2964621a7a
Marianne Schaefer <marianne.m.schaefer@gmail.com>
2021-04-19 09:47:00 -0400

    Update StringUtils to be able to capitalize.

--- src/org/etools/j1939_84/utils/StringUtils.java
+++ src/org/etools/j1939_84/utils/StringUtils.java
@@ -76,4 +76,18 @@ public class StringUtils {
         }
     }
 
+    public static String capitalizeString(String string) {
+        char[] chars = string.toLowerCase().toCharArray();
+        boolean found = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!found && Character.isLetter(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                found = true;
+            } else if (Character.isWhitespace(chars[i]) || chars[i] == '.' || chars[i] == '\'') {
+                found = false;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
 }
