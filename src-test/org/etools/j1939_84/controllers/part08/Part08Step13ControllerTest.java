f30fa31ea48162670aa0ab54a62c407a80cb4b4b
Joe Batt <Joe@SolidDesign.net>
2022-09-14 22:43:05 -0400

    6.8.13.2 rewrite

--- src-test/org/etools/j1939_84/controllers/part08/Part08Step13ControllerTest.java
+++ src-test/org/etools/j1939_84/controllers/part08/Part08Step13ControllerTest.java
@@ -23,6 +23,7 @@ import org.etools.j1939_84.controllers.SectionA5Verifier;
 import org.etools.j1939_84.controllers.StepController;
 import org.etools.j1939_84.controllers.TestResultsListener;
 import org.etools.j1939_84.model.OBDModuleInformation;
+import org.etools.j1939_84.model.Outcome;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.ReportFileModule;
@@ -31,6 +32,7 @@ import org.etools.j1939_84.modules.VehicleInformationModule;
 import org.etools.j1939_84.utils.AbstractControllerTest;
 import org.etools.j1939tools.j1939.J1939;
 import org.etools.j1939tools.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939tools.j1939.packets.AcknowledgmentPacket.Response;
 import org.etools.j1939tools.modules.CommunicationsModule;
 import org.etools.j1939tools.modules.DateTimeModule;
 import org.junit.After;
@@ -141,7 +143,7 @@ public class Part08Step13ControllerTest extends AbstractControllerTest {
     public void testHappyPathNoFailures() {
         dataRepository.putObdModule(new OBDModuleInformation(0));
 
-        var nack = AcknowledgmentPacket.create(0, NACK);
+        var nack = AcknowledgmentPacket.create(0, Response.DENIED);
         when(communicationsModule.requestDM3(any(), eq(0))).thenReturn(List.of(nack));
 
         runTest();
@@ -151,7 +153,7 @@ public class Part08Step13ControllerTest extends AbstractControllerTest {
         verify(communicationsModule).requestDM3(any(), eq(0));
         verify(communicationsModule).requestDM3(any());
 
-        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.2.a"));
+        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.2.b"));
         verify(verifier).verifyDataNotErased(any(), eq("6.8.13.4.a"));
 
         assertEquals(10000, dateTimeModule.getTimeAsLong());
@@ -172,6 +174,42 @@ public class Part08Step13ControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    public void test_6_8_13_2_c() {
+        dataRepository.putObdModule(new OBDModuleInformation(0));
+
+        var nack = AcknowledgmentPacket.create(0, Response.NACK);
+        when(communicationsModule.requestDM3(any(), eq(0))).thenReturn(List.of(nack));
+
+        runTest();
+
+        verify(verifier).setJ1939(j1939);
+
+        verify(communicationsModule).requestDM3(any(), eq(0));
+        verify(communicationsModule).requestDM3(any());
+
+        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.2.b"));
+        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.4.a"));
+
+        assertEquals(10000, dateTimeModule.getTimeAsLong());
+
+        String expected = "";
+        expected += "Step 6.8.13.1.b - Waiting 5 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.1.b - Waiting 4 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.1.b - Waiting 3 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.1.b - Waiting 2 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.1.b - Waiting 1 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.3.b - Waiting 5 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.3.b - Waiting 4 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.3.b - Waiting 3 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.3.b - Waiting 2 seconds before checking for erased information" + NL;
+        expected += "Step 6.8.13.3.b - Waiting 1 seconds before checking for erased information";
+        assertEquals(expected, listener.getMessages());
+        assertEquals("", listener.getResults());
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, Outcome.WARN,
+                                        "6.8.13.2.c - OBD ECU Engine #1 (0) did provide a NACK with control byte = 3 for the DS query");
+  }
+
+    @Test
     public void testFailureForNoNACK() {
         dataRepository.putObdModule(new OBDModuleInformation(0));
         dataRepository.putObdModule(new OBDModuleInformation(1));
@@ -188,7 +226,7 @@ public class Part08Step13ControllerTest extends AbstractControllerTest {
         verify(communicationsModule).requestDM3(any(), eq(1));
         verify(communicationsModule).requestDM3(any());
 
-        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.2.a"));
+        verify(verifier).verifyDataNotErased(any(), eq("6.8.13.2.b"));
         verify(verifier).verifyDataNotErased(any(), eq("6.8.13.4.a"));
 
         assertEquals(10000, dateTimeModule.getTimeAsLong());
@@ -207,8 +245,8 @@ public class Part08Step13ControllerTest extends AbstractControllerTest {
         assertEquals(expected, listener.getMessages());
         assertEquals("", listener.getResults());
 
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.8.13.2.a - Engine #1 (0) did not NACK");
-        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.8.13.2.a - Engine #2 (1) did not NACK");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.8.13.2.a - OBD ECU Engine #1 (0) did not provide a NACK for the DS query");
+        verify(mockListener).addOutcome(PART_NUMBER, STEP_NUMBER, FAIL, "6.8.13.2.a - OBD ECU Engine #2 (1) did not provide a NACK for the DS query");
     }
 
 }
--- src/org/etools/j1939_84/controllers/part08/Part08Step13Controller.java
+++ src/org/etools/j1939_84/controllers/part08/Part08Step13Controller.java
@@ -3,10 +3,16 @@
  */
 package org.etools.j1939_84.controllers.part08;
 
+import static org.etools.j1939tools.j1939.packets.AcknowledgmentPacket.Response.BUSY;
+import static org.etools.j1939tools.j1939.packets.AcknowledgmentPacket.Response.DENIED;
 import static org.etools.j1939tools.j1939.packets.AcknowledgmentPacket.Response.NACK;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import org.etools.j1939_84.controllers.DataRepository;
 import org.etools.j1939_84.controllers.SectionA5Verifier;
@@ -15,7 +21,9 @@ import org.etools.j1939_84.model.OBDModuleInformation;
 import org.etools.j1939_84.modules.BannerModule;
 import org.etools.j1939_84.modules.EngineSpeedModule;
 import org.etools.j1939_84.modules.VehicleInformationModule;
+import org.etools.j1939tools.j1939.Lookup;
 import org.etools.j1939tools.j1939.packets.AcknowledgmentPacket;
+import org.etools.j1939tools.j1939.packets.ParsedPacket;
 import org.etools.j1939tools.modules.CommunicationsModule;
 import org.etools.j1939tools.modules.DateTimeModule;
 
@@ -66,23 +74,43 @@ public class Part08Step13Controller extends StepController {
         verifier.setJ1939(getJ1939());
 
         // 6.8.13.1.a. DS DM3 [(send Request (PGN 59904) for PGN 65228]) to each OBD ECU.
-        for (OBDModuleInformation obdInfo : getDataRepository().getObdModules()) {
-
-            // 6.8.13.2.a. Fail if any ECU does not NACK
-            boolean nacked = getCommunicationsModule().requestDM3(getListener(), obdInfo.getSourceAddress())
-                                                      .stream()
-                                                      .map(AcknowledgmentPacket::getResponse)
-                                                      .anyMatch(r -> r == NACK);
-            if (!nacked) {
-                addFailure("6.8.13.2.a - " + obdInfo.getModuleName() + " did not NACK");
-            }
-        }
+        var dsPackets = getDataRepository().getObdModuleAddresses()
+                .stream()
+                .map(a -> getCommunicationsModule().requestDM3(getListener(), a))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
 
         // 6.8.13.1.b. Wait 5 seconds before checking for erased information.
         pause("Step 6.8.13.1.b - Waiting %1$d seconds before checking for erased information", 5);
 
-        // 6.8.13.2.a. Fail if any diagnostic information erased.
-        verifier.verifyDataNotErased(getListener(), "6.8.13.2.a");
+        // 6.8.13.2.a. Fail if any ECU does not NACK with control byte = 1 or 2 or 3.
+        List<Integer> addresses1 = new ArrayList<>(getDataRepository().getObdModuleAddresses());
+        dsPackets.stream()
+                 .filter(a2 -> a2.getResponse() == NACK || a2.getResponse() == BUSY || a2.getResponse() == DENIED)
+                 .map(ParsedPacket::getSourceAddress)
+                 .forEach(addresses1::remove);
+
+        addresses1.stream()
+                  .distinct()
+                  .sorted()
+                  .map(Lookup::getAddressName)
+                  .map(moduleName1 -> "6.8.13.2.a" + " - OBD ECU " + moduleName1
+                          + " did not provide a NACK for the DS query")
+                  .forEach(this::addFailure);
+
+        // 6.8.13.2.b. Fail if any diagnostic information erased.
+        verifier.verifyDataNotErased(getListener(), "6.8.13.2.b");
+
+        // 6.8.13.2.c Warn if any OBD ECU NACKs with control byte = 3
+        dsPackets.stream()
+                 .filter(a1 -> a1.getResponse() == NACK)
+                 .map(ParsedPacket::getSourceAddress)
+                 .distinct()
+                 .sorted()
+                 .map(Lookup::getAddressName)
+                 .map(moduleName -> "6.8.13.2.c" + " - OBD ECU " + moduleName
+                         + " did provide a NACK with control byte = 3 for the DS query")
+                 .forEach(this::addWarning);
 
         // 6.8.13.3.a. Global DM3.
         getCommunicationsModule().requestDM3(getListener());
