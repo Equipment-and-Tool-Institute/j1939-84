7d13891e8d069fe38cd60cd87739aa33e67f0fef
Karys Grundman <karys@soliddesign.net>
2024-12-19 13:37:41 -0500

    fix #1371 collect DM12s from all ECUs in OBD System

--- src/org/etools/j1939_84/controllers/part04/Part04Step10Controller.java
+++ src/org/etools/j1939_84/controllers/part04/Part04Step10Controller.java
@@ -87,7 +87,10 @@ public class Part04Step10Controller extends StepController {
                   .stream()
                   .map(f -> f.getDtc())
                   .collect(Collectors.toList());
-            List<DiagnosticTroubleCode> dm12DTCs = getDTCs(p.getSourceAddress());
+            List<DiagnosticTroubleCode> dm12DTCs = getDataRepository().getObdModuleAddresses().stream()
+                    .map(addr -> getDTCs(addr))
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
             if (ffDTCs.stream().filter(dtc -> dm12DTCs.contains(dtc)).findFirst().isEmpty()) {
                 addFailure("6.4.10.2.b - " + p.getModuleName()
                         + " did not report DTC in freeze frame data which included any DTC reported in DM12 earlier in this part");
